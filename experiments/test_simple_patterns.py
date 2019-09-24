9171ff0dc3fb5daf767fdb6457d6b41daafd9b89
Marcel Schwittlick <marzzzel@gmail.com>
2019-09-24 18:32:38 +0200

    adds simple line sketch for testing

---
+++ experiments/test_simple_patterns.py
@@ -0,0 +1,45 @@
+from cursor import loader
+from cursor import renderer
+from cursor import path
+from cursor import data
+
+def test_simple_pattern1():
+    p = data.DataHandler().recordings()
+    l = loader.Loader(directory=p, limit_files=1)
+    rec = l.single(0)
+
+    r = renderer.GCodeRenderer('straight_lines', z_down=3.0)
+    jpeg_renderer = renderer.JpegRenderer('straight_lines')
+
+    coll = path.PathCollection(rec.resolution)
+
+    xoffset = 200
+    yoffset = 100
+
+    for i in range(50):
+        print(i)
+        xfrom = 1.5 * i + xoffset
+        yfrom = 5 + yoffset
+        xto = 1.5 * i + xoffset
+        yto = 200 + yoffset
+
+        pth = path.Path()
+        pth.add(xfrom, yfrom, 0)
+        pth.add(xto, yto, 0)
+
+        coll.add(pth, rec.resolution)
+
+    bb = coll.bb()
+
+    coll.add(path.Path([path.TimedPosition(bb.x, bb.y), path.TimedPosition(bb.x + bb.w, bb.y)]), rec.resolution)
+    coll.add(path.Path([path.TimedPosition(bb.x + bb.w, bb.y), path.TimedPosition(bb.x + bb.w, bb.y + bb.h)]), rec.resolution)
+    coll.add(path.Path([path.TimedPosition(bb.x + bb.w, bb.y + bb.h), path.TimedPosition(bb.x, bb.y + bb.h)]), rec.resolution)
+    coll.add(path.Path([path.TimedPosition(bb.x, bb.y + bb.h), path.TimedPosition(bb.x, bb.y)]), rec.resolution)
+
+    coll.fit(path.Paper.a1_landscape(), 50)
+
+    r.render(coll, F'straight_lines_{coll.hash()}')
+    jpeg_renderer.render(coll, F'straight_lines_{coll.hash()}')
+
+if __name__ == '__main__':
+    test_simple_pattern1()
