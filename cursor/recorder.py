c4604fcaf04454ddefb8826f0423d80dbda01ba6
Marcel Schwittlick <marzzzel@gmail.com>
2020-03-22 21:45:23 +0100

    adds proof on concept for recorder with ui - creates tray icon with stop button - starts and stops recorder safely - tested on macos

--- cursor/recorder.py
+++ cursor/recorder.py
@@ -1,47 +1,50 @@
+from cursor import path
+from cursor import data
+
 import time
 import datetime
 import pytz
 import atexit
 import os
 import pyautogui
+import wasabi
+import pathlib
+import pystray
 from pynput.mouse import Listener as MouseListener
 from pynput.keyboard import Listener as KeyboardListener
 
-from cursor import path
-from cursor import data
+log = wasabi.Printer()
 
 
 class CursorRecorder:
-    SAVE_PATH = data.DataHandler().recordings()
     keyboard_recodings = []
     current_line = path.Path()
     started = False
     start_time_stamp = None
-    current = set()
     recorder = None
     running = True
 
     def __init__(self):
-        print(self.SAVE_PATH)
-
         self.start_time_stamp = self._get_utc_timestamp()
         atexit.register(self.save)
         self.mouse_recordings = path.PathCollection(pyautogui.size())
 
+        log.good("Setting up mouse hook")
         self.mouse_listener = MouseListener(
             on_move=self.on_move, on_click=self.on_click
         )
         self.mouse_listener.start()
 
+        log.good("Setting up keyboard hook")
         self.key_listener = KeyboardListener(
             on_press=self.on_press, on_release=self.on_release
         )
         self.key_listener.start()
 
-        print("Running recorder.. Saving to " + self.SAVE_PATH.as_posix())
+        log.good("Started cursor recorder")
 
-        while True:
-            time.sleep(0.01)
+        #while True:
+        #    time.sleep(0.01)
 
     def toggle(self):
         if not self.running:
@@ -59,7 +62,6 @@ class CursorRecorder:
         return utc_timestamp
 
     def on_move(self, x, y):
-        print("move " + str(x))
         _x = x / self.mouse_recordings.resolution.width
         _y = y / self.mouse_recordings.resolution.height
         self.current_line.add(_x, _y, self._get_utc_timestamp())
@@ -77,38 +79,26 @@ class CursorRecorder:
             self.current_line.clear()
 
     def on_press(self, btn):
-        print("press " + str(btn))
         pass
-        # in case this is a list of keys, for a combination
-        # if btn in self.START_STOP_COMBINATION:
-        #    self.current.add(btn)
-        #    if all(k in self.current for k in self.START_STOP_COMBINATION):
-        #        self.toggle()
 
     def on_release(self, btn):
-        print("release " + str(btn))
-        try:
-            self.current.remove(btn)
-            return
-        except KeyError:
-            pass
         try:
             self.keyboard_recodings.append((btn.char, self._get_utc_timestamp()))
         except AttributeError as ae:
-            print(ae)
+            log.fail(f"Couldn't save key because of {ae}")
 
     def save(self):
-        print("save")
-        if not os.path.exists(self.SAVE_PATH):
-            os.makedirs(self.SAVE_PATH)
+        save_path = data.DataHandler().recordings()
+        pathlib.Path(save_path).mkdir(parents=True, exist_ok=True)
 
         recs = {"mouse": self.mouse_recordings, "keys": self.keyboard_recodings}
 
         fname_compressed = os.path.join(
-            self.SAVE_PATH, str(self.start_time_stamp) + "_compressed.json"
+            save_path, str(self.start_time_stamp) + "_compressed.json"
         )
-
-        print(f"saving {fname_compressed}")
+        log.good(f"Saving mouse recordings: {len(self.mouse_recordings)}")
+        log.good(f"Saving keyboard recordings: {len(self.keyboard_recodings)}")
+        log.good(f"Saving compressed to {fname_compressed}")
 
         with open(fname_compressed, "w") as fp:
             dump = path.JsonCompressor().json_zip(recs)
@@ -120,5 +110,31 @@ class CursorRecorder:
         #     fp.write(dump)
 
 
+from PIL import Image
+rr = None
+
+def load_image():
+    im = Image.open("mouse-icon.gif")
+    return im
+
+def clicked(icon, item):
+    global rr
+    icon.stop()
+
+def after(icon):
+    global rr
+    rr = CursorRecorder()
+    icon.visible = True
+
+
+def create_menu():
+    image = load_image()
+    icon = pystray.Icon(
+        "Cursor", image, menu=pystray.Menu(pystray.MenuItem("Stop", clicked))
+    )
+    log.info("created menu")
+    icon.run(after)
+
 def runRecorder():
-    CursorRecorder()
+    create_menu()
+    #
