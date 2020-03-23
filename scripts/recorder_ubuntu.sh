ab88deb75307c5a94263ae25d95fdc3dc0bc7488
Marcel Schwittlick <marzzzel@gmail.com>
2020-03-23 18:19:31 +0100

    adds some documentation to recorder run scripts

--- scripts/recorder_ubuntu.sh
+++ scripts/recorder_ubuntu.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# make sure to run this only with an activated venv
+
 echo "setup requirements"
 pip install -r ../requirements.txt --quiet
 pip install -e .. --quiet
--- scripts/recorder_win.bat
+++ scripts/recorder_win.bat
@@ -1,3 +1,3 @@
 CALL ..\venv\Scripts\activate
-CALL pip install -e ..
+CALL pip install -e .. --quiet
 CALL cursor_recorder
