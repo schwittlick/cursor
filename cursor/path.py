153452836a78c8d46a61a55327c1f6cc64d86b5b
Marcel Schwittlick <marzzzel@gmail.com>
2020-04-05 17:57:49 +0200

    refactors paper/machine properties to new device module. also fixes stuff in the other experiments, old stuff

--- cursor/path.py
+++ cursor/path.py
@@ -8,30 +8,6 @@ import random
 import hashlib
 
 
-class Paper:
-    X_FACTOR = 2.91666
-    Y_FACTOR = 2.83333
-
-    CUSTOM_36_48 = (360 * X_FACTOR, 480 * Y_FACTOR)
-    CUSTOM_48_36 = (480 * X_FACTOR, 360 * Y_FACTOR)
-    DIN_A1_LANDSCAPE = (841 * X_FACTOR, 594 * Y_FACTOR)
-    DIN_A0_LANDSCAPE = (1189 * X_FACTOR, 841 * Y_FACTOR)
-
-    @staticmethod
-    def custom_36_48_portrait():
-        return Paper.CUSTOM_36_48
-
-    @staticmethod
-    def custom_36_48_landscape():
-        return Paper.CUSTOM_48_36
-
-    @staticmethod
-    def a1_landscape():
-        return Paper.DIN_A1_LANDSCAPE
-
-    @staticmethod
-    def a0_landscape():
-        return Paper.DIN_A0_LANDSCAPE
 
 
 class TimedPosition:
--- experiments/composition37.py
+++ experiments/composition37.py
@@ -2,13 +2,14 @@ from cursor import loader
 from cursor import renderer
 from cursor import path
 from cursor import data
+from cursor import device
 
 
 def composition37(p0, p1, offset):
     gcode_renderer = renderer.GCodeRenderer("composition37", z_down=3.0)
     jpeg_renderer = renderer.JpegRenderer("composition37")
 
-    coll = path.PathCollection(rec.resolution)
+    coll = path.PathCollection()
 
     start = (100, 100)
     end = (400, 100)
@@ -21,21 +22,21 @@ def composition37(p0, p1, offset):
 
     bottompath.reverse()
 
-    coll.add(toppath, rec.resolution)
-    coll.add(bottompath, rec.resolution)
+    coll.add(toppath)
+    coll.add(bottompath)
 
     p1_morphed = p1.morph(toppath[0].pos(), bottompath[0].pos())
-    coll.add(p1_morphed, rec.resolution)
+    coll.add(p1_morphed)
 
     for i in range(len(toppath)):
         st = toppath[i]
         en = bottompath[i]
         newpath = p1.morph(en.pos(), st.pos())
-        coll.add(newpath, rec.resolution)
+        coll.add(newpath)
 
     print(coll.bb())
 
-    coll.fit(path.Paper.a0_landscape(), 200)
+    coll.fit(device.DrawingMachine.Paper.a0_landscape(), 200)
 
     print(coll.bb())
 
@@ -46,10 +47,8 @@ def composition37(p0, p1, offset):
 if __name__ == "__main__":
     p = data.DataDirHandler().recordings()
     ll = loader.Loader(directory=p, limit_files=5)
-    rec = ll.single(0)
     all_paths = ll.all_paths()
 
-    # print(len(all_paths))
     for i in range(1):
         print(f"Creating Composition #37 with offset={i}")
         p0 = all_paths[i]
