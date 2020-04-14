ef86bb898ae74ceaed4e91006cf09f16b3d823bb
Marcel Schwittlick <marzzzel@gmail.com>
2020-04-15 00:21:14 +0200

    fixes kivy gui for opening a file chooser. more complicated than necessary.. again

--- experiments/kivy_gui.py
+++ experiments/kivy_gui.py
@@ -10,36 +10,25 @@ import kivy
 from kivy.app import App
 from kivy.uix.label import Label
 from kivy.uix.gridlayout import GridLayout
+from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
-from kivy.uix.floatlayout import FloatLayout
-from kivy.properties import ObjectProperty
 from kivy.uix.progressbar import ProgressBar
 from kivy.uix.popup import Popup
 from kivy.uix.filechooser import FileChooserListView
-from kivy.uix.dropdown import DropDown
-from kivy.lang import Builder
 
 kivy.require("2.0.0")
 
 log = wasabi.Printer()
 
-p = pathlib.Path(__file__).resolve().parent.joinpath("kivy_gui.kv")
-Builder.load_file(p.as_posix())
-
-class LoadDialog(FloatLayout):
-    load = ObjectProperty(None)
-    cancel = ObjectProperty(None)
+#p = pathlib.Path(__file__).resolve().parent.joinpath("kivy_gui.kv")
+#   Builder.load_file(p.as_posix())
 
 
 class MyGrid(GridLayout):
 
-    def __init__(self, **kwargs):
-        super(MyGrid, self).__init__(**kwargs)
-
-        #self.loadfile = ObjectProperty(None)
-        self.fs = FileChooserListView()
-
+    selected_file = None
+    def init_connect_area(self):
         self.machine = DrawingMachine()
         self.cols = 1
         self.connect_area = GridLayout()
@@ -59,28 +48,27 @@ class MyGrid(GridLayout):
 
         self.add_widget(self.connect_area)
 
+    def init_file_area(self):
         self.file_status_area = GridLayout()
         self.file_status_area.cols = 1
         self.file_status_area.disabled = False
-        #self.gcode_file_path = TextInput(
+        # self.gcode_file_path = TextInput(
         #    text="H:\cursor\data\experiments\composition57\gcode\composition57_de7c4e2b608a403be7a96e077f6aeab5.nc",
         #    multiline=False,
-        #)
+        # )
         self.find_button = Button(text="find", font_size=20)
         self.find_button.bind(on_press=self.find)
 
         self.file_status_area.add_widget(self.find_button)
-
-        self.load_button = Button(text="load", font_size=20)
-        self.load_button.bind(on_press=self.load)
-        self.file_status_area.add_widget(self.load_button)
         self.add_widget(self.file_status_area)
 
+    def init_preview_area(self):
         self.preview_area = GridLayout()
         self.preview_area.cols = 4
         self.preview_area.disabled = True
         self.add_widget(self.preview_area)
 
+    def init_stream_area(self):
         self.start_pause_area = GridLayout()
         self.start_pause_area.cols = 5
         self.start_pause_area.disabled = True
@@ -100,6 +88,50 @@ class MyGrid(GridLayout):
 
         self.progress = ProgressBar(max=1000)
         self.add_widget(self.progress)
+
+    def find(self, instance):
+        bl = BoxLayout()
+        self.fs = FileChooserListView(path=".")
+        bl.add_widget(self.fs)
+
+        self.load = Button(text="load", font_size=20)
+        self.load.bind(on_press=self.ffind)
+        bl.add_widget(self.load)
+
+        self._popup = Popup(title="Load file", content=bl,
+                            size_hint=(0.9, 0.9), auto_dismiss=False)
+        self._popup.open()
+
+    def ffind(self, el):
+        path = self.fs.selection[0]
+        print(self.fs.selection)
+        self.dismiss_popup()
+
+        abspath = pathlib.Path(path)
+
+        if not abspath.is_file():
+            log.warn("Not a file..")
+            return
+        else:
+            log.good(f"Loaded {path}")
+
+        c = None
+        with open(abspath.as_posix()) as content:
+            c = content.readlines()
+        log.good(f"lines: {len(c)}")
+
+        self.progress.max = len(c)
+
+    def dismiss_popup(self):
+        self._popup.dismiss()
+
+    def __init__(self, **kwargs):
+        super(MyGrid, self).__init__(**kwargs)
+
+        self.init_connect_area()
+        self.init_file_area()
+        self.init_preview_area()
+        self.init_stream_area()
 
     def _start(self, instance):
         self.machine.stream(self.gcode_file_path.text)
@@ -131,42 +163,6 @@ class MyGrid(GridLayout):
         for i in range(self.progress.max):
             self.progress.value = i
             time.sleep(0.00001)
-
-    def find(self, instance):
-        #self.add_widget(self.fs)
-        self.fs.load = self.ffind
-        self.fs.cancel = self.dismiss_popup
-        #content = LoadDialog(load=self.ffind, cancel=self.dismiss_popup)
-        self._popup = Popup(title="Load file", content=self.fs,
-                            size_hint=(0.9, 0.9))
-        self._popup.open()
-
-    def ffind(self):
-        print("ffind")
-        pass
-
-    def dismiss_popup(self):
-        self._popup.dismiss()
-
-    def load(self, instance):
-        path = self.gcode_file_path.text
-        abspath = pathlib.Path(path)
-
-        if not abspath.is_file():
-            log.warn("Not a file..")
-            return
-        else:
-            log.good(f"Loaded {path}")
-
-        c = None
-        with open(abspath.as_posix()) as content:
-            c = content.readlines()
-        log.good(f"lines: {len(c)}")
-
-        self.progress.max = len(c)
-        # thread1 = threading.Thread(target=self.simulate_progress)
-        # thread1.start()
-
 
 class MyApp(App):
     def build(self):
