f15914d172da67c70a58c98331e5620227d79952
Marcel Schwittlick <marzzzel@gmail.com>
2020-07-06 15:19:38 +0200

    fixes orientation of c37

--- experiments/composition37.py
+++ experiments/composition37.py
@@ -1,21 +1,24 @@
 from cursor import loader
 from cursor import renderer
+from cursor import filter
 from cursor import path
 from cursor import data
 from cursor import device
 
 
 def composition37(p0, p1, offset):
-    gcode_renderer = renderer.GCodeRenderer("composition37", z_down=3.0)
-    jpeg_renderer = renderer.JpegRenderer("composition37")
+    gcode_folder = data.DataDirHandler().gcode("composition37")
+    folder = data.DataDirHandler().jpg("composition37")
+    gcode_renderer = renderer.GCodeRenderer(gcode_folder, z_down=3.0)
+    jpeg_renderer = renderer.JpegRenderer(folder)
 
     coll = path.PathCollection()
 
     start = (100, 100)
-    end = (400, 100)
+    end = (100, 700)
 
-    startbottom = (100, 600)
-    endbottom = (400, 600)
+    startbottom = (400, 100)
+    endbottom = (400, 700)
 
     toppath = p0.morph(start, end)
     bottompath = p0.morph(endbottom, startbottom)
@@ -36,21 +39,35 @@ def composition37(p0, p1, offset):
 
     print(coll.bb())
 
-    coll.fit(device.DrawingMachine.Paper.a0_landscape(), 200)
+    coll.fit(device.DrawingMachine.Paper.a1_landscape(), 100)
 
     print(coll.bb())
 
-    gcode_renderer.render(coll, f"composition37_{offset}")
-    jpeg_renderer.render(coll, f"composition37_{offset}")
+    fname = f"composition37_broken_{offset}"
+    gcode_renderer.render(coll)
+    gcode_renderer.save(fname)
+    jpeg_renderer.render(coll)
+    jpeg_renderer.save(fname)
 
 
 if __name__ == "__main__":
     p = data.DataDirHandler().recordings()
-    ll = loader.Loader(directory=p, limit_files=5)
+    ll = loader.Loader(directory=p, limit_files=15)
     all_paths = ll.all_paths()
 
-    for i in range(1):
-        print(f"Creating Composition #37 with offset={i}")
-        p0 = all_paths[i]
-        p1 = all_paths[i + 1]
+    entropy_filter = filter.EntropyFilter(2.5, 2.5)
+    all_paths.filter(entropy_filter)
+
+    mincount_filter = filter.MinPointCountFilter(20)
+    all_paths.filter(mincount_filter)
+
+    maxcount_filter = filter.MaxPointCountFilter(100)
+    all_paths.filter(maxcount_filter)
+
+    for i in range(100):
+        import random
+        r = random.randint(0, len(all_paths) - 1)
+        print(f"Creating Composition #37 with offset={r}")
+        p0 = all_paths[r]
+        p1 = all_paths[r + 1]
         composition37(p0, p1, i)
