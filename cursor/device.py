7d31fa88d295ea1d331d8f84c3c1065b9e1d442f
Marcel Schwittlick <marzzzel@gmail.com>
2020-07-12 18:32:08 +0200

    adds dimension for new paper size

--- cursor/device.py
+++ cursor/device.py
@@ -136,6 +136,8 @@ class DrawingMachine:
 
         CUSTOM_36_48 = (360 * X_FACTOR, 480 * Y_FACTOR)
         CUSTOM_48_36 = (480 * X_FACTOR, 360 * Y_FACTOR)
+        CUSTOM_42_56 = (420 * X_FACTOR, 560 * Y_FACTOR)
+        CUSTOM_56_42 = (560 * X_FACTOR, 420 * Y_FACTOR)
         DIN_A1_LANDSCAPE = (841 * X_FACTOR, 594 * Y_FACTOR)
         DIN_A0_LANDSCAPE = (1189 * X_FACTOR, 841 * Y_FACTOR)
 
@@ -146,6 +148,14 @@ class DrawingMachine:
         @staticmethod
         def custom_36_48_landscape():
             return DrawingMachine.Paper.CUSTOM_48_36
+
+        @staticmethod
+        def custom_42_56_portrait():
+            return DrawingMachine.Paper.CUSTOM_42_56
+
+        @staticmethod
+        def custom_42_56_landscape():
+            return DrawingMachine.Paper.CUSTOM_56_42
 
         @staticmethod
         def a1_landscape():
