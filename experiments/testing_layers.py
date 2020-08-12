0763e27d128c46f0559d818c74fce5a88fe0d651
Marcel Schwittlick <info@schwittlick.net>
2020-08-12 18:08:36 +0200

    fixes fitting problem

--- experiments/testing_layers.py
+++ experiments/testing_layers.py
@@ -3,7 +3,6 @@ from cursor import path
 from cursor import data
 from cursor import device
 
-import random
 import math
 
 
@@ -12,7 +11,7 @@ def full_spiral(pp):
     yextra = 0
     # r = 1
     r = 50
-    while theta < math.pi * 3 * 80:  # 80
+    while theta < math.pi * 200:  # 80
         y = r * math.cos(theta) * 2
         x = r * math.sin(theta) + yextra
         pp.add(x, y, 0)
@@ -34,16 +33,9 @@ if __name__ == "__main__":
     coll.add(pp)
     coll.add(reversed_path)
 
-    coll.fit(device.DrawingMachine.Paper.a1_landscape(), 90)
+    coll.fit(device.DrawingMachine.Paper.a1_landscape(), padding_mm=90)
 
     fname = f"composition59_{num}_a1"
-
-    #jpeg_renderer.render(coll)
-    #jpeg_renderer.save(f"{fname}")
-    #gcode_renderer.render(coll)
-    #gcode_renderer.save(f"{fname}")
-
-    #exit(0)
 
     separate_layers = coll.get_layers()
     for layer, pc in separate_layers.items():
@@ -51,8 +43,6 @@ if __name__ == "__main__":
         folder = data.DataDirHandler().jpg("testing_layers")
         gcode_renderer = renderer.GCodeRenderer(gcode_folder, z_down=4.5)
         jpeg_renderer = renderer.JpegRenderer(folder)
-
-        pc.fit(device.DrawingMachine.Paper.a1_landscape(), 90)
 
         jpeg_renderer.render(pc)
         jpeg_renderer.save(f"{fname}_{layer}")
