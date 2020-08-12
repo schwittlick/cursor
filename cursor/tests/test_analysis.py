7d56d0827bee1a07fcd58e77cb57a6ba3d75568e
Marcel Schwittlick <info@schwittlick.net>
2020-08-12 18:11:07 +0200

    linting

--- cursor/tests/test_analysis.py
+++ cursor/tests/test_analysis.py
@@ -10,37 +10,34 @@ import random
 def test_simple_histogram():
     h = Histogram()
 
-
     # path direction change histogram
-    #p = Path()
-    #for i in range(10000):
+    # p = Path()
+    # for i in range(10000):
     #    r1 = random.randint(-100, 100)
     #    r2 = random.randint(-100, 100)
     #    p.add(x=r1, y=r2)
 
-    #h.get(p.direction_changes())
-
+    # h.get(p.direction_changes())
 
     dir = DataDirHandler().recordings()
     single_file = dir.joinpath("1565088885.39372_compressed.json")
     ll = Loader(directory=dir, limit_files=2)
-    #ll.load_file(single_file)
-
+    # ll.load_file(single_file)
 
     shannxs = []
     shannys = []
     shannchan = []
     pps = ll.all_paths()
     for p2 in pps:
-        #print(p2)
-        #changes = p2.direction_changes()
+        # print(p2)
+        # changes = p2.direction_changes()
         shannxs.append(p2.shannon_x)
         shannys.append(p2.shannon_y)
         shannchan.append(p2.shannon_direction_changes)
-        #print(changes)
-        #hist, bins = h.get(changes)
-        #print("1")
-#        exit(0)
+        # print(changes)
+        # hist, bins = h.get(changes)
+        # print("1")
+    #        exit(0)
 
     histx, binsx = h.get(shannxs)
     histy, binsy = h.get(shannys)
