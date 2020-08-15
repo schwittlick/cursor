ccc7578bbdc0feb2bccf4c11d4b6649f3b6844f1
Marcel Schwittlick <marzzzel@gmail.com>
2020-08-15 03:31:54 +0200

    clean test file

--- cursor/tests/test_analysis.py
+++ cursor/tests/test_analysis.py
@@ -1,10 +1,6 @@
 from cursor.analysis import Histogram
 from cursor.data import DataDirHandler
 from cursor.loader import Loader
-from cursor.path import PathCollection
-from cursor.path import Path
-
-import random
 
 
 def test_simple_histogram():
