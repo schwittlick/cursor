11a9f3cc7929788cabcca9e4d36c411b39d55cc5
Marcel Schwittlick <info@schwittlick.net>
2021-02-09 20:15:55 +0100

    adds optimization to c56

--- experiments/composition65.py
+++ experiments/composition65.py
@@ -15,11 +15,13 @@ def save_wrapper(pc, projname, fname):
 
 if __name__ == "__main__":
     recs = data.DataDirHandler().recordings()
-    loader = loader.Loader(directory=recs, limit_files=5)
+    loader = loader.Loader(directory=recs, limit_files=20)
     all_paths = loader.all_paths()
 
     sorter1 = filter.DistanceFilter(0.05)
     all_paths.filter(sorter1)
+
+    all_paths.limit()
 
     #sorter2 = filter.MaxPointCountFilter(50)
     #all_paths.filter(sorter2)
@@ -32,6 +34,9 @@ if __name__ == "__main__":
         pc.add(p)
 
         counter += 1
+        counter = counter % 6
+
+    pc.reorder_quadrants(10, 10)
 
     device.SimpleExportWrapper().ex(
         pc,
@@ -39,5 +44,5 @@ if __name__ == "__main__":
         device.PaperSize.LANDSCAPE_A1,
         50,
         "composition65",
-        f"c65{pc.hash()}",
+        f"c65{pc.hash()}"
     )
