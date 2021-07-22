5a9fd597430cd5969be0a01129b6f6494ce5d42e
Marcel Schwittlick <info@schwittlick.net>
2021-07-22 22:43:14 +0200

    formatting rust

--- cursor-rust/src/recorder.rs
+++ cursor-rust/src/recorder.rs
@@ -1,12 +1,12 @@
-use winapi::shared::windef::POINT;
-use std::thread;
-use std::fmt;
 use chrono::{DateTime, Utc}; // 0.4.15
-use std::time::{Duration, SystemTime};
 use rdev::display_size;
 use rdev::{listen, Event};
+use std::fmt;
 use std::fs::File;
 use std::io::prelude::*;
+use std::thread;
+use std::time::{Duration, SystemTime};
+use winapi::shared::windef::POINT;
 
 fn get_mouse_pos() -> (i32, i32) {
     let mut point = POINT { x: 0, y: 0 };
@@ -14,8 +14,7 @@ fn get_mouse_pos() -> (i32, i32) {
     (point.x, point.y)
 }
 
-fn get_resolution() -> (i32, i32)
-{
+fn get_resolution() -> (i32, i32) {
     let mut _r = unsafe {
         let _dpi_aware = ::winapi::um::winuser::SetProcessDPIAware();
         let x = ::winapi::um::winuser::GetSystemMetrics(::winapi::um::winuser::SM_CXVIRTUALSCREEN);
@@ -25,16 +24,13 @@ fn get_resolution() -> (i32, i32)
     return (_r.0, _r.1);
 }
 
-struct TimedPoint 
-{
+struct TimedPoint {
     x: f64,
     y: f64,
-    time: DateTime<Utc>
+    time: DateTime<Utc>,
 }
 
-
-fn main()
-{
+fn main() {
     ctrlc::set_handler(move || {
         println!("cleaning up.");
         std::process::exit(0);
@@ -44,10 +40,9 @@ fn main()
     record();
 }
 
-
 impl fmt::Display for TimedPoint {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-       // let string = format!("({x.*}, {}, {})", 3, x=self.x, y=self.y, time=self.time.to_rfc3339());
+        // let string = format!("({x.*}, {}, {})", 3, x=self.x, y=self.y, time=self.time.to_rfc3339());
         write!(f, "({}, {}, {})", self.x, self.y, self.time.to_rfc3339())
     }
 }
@@ -59,8 +54,7 @@ fn callback(event: Event) {
 fn write_points(point: &Vec<TimedPoint>) -> std::io::Result<()> {
     let mut f = File::create("foo.txt")?;
 
-    for p in point.iter()
-    {
+    for p in point.iter() {
         let str = p.to_string() + "\n";
         f.write_all(str.as_bytes()).expect("couldnt write :(");
     }
@@ -69,8 +63,7 @@ fn write_points(point: &Vec<TimedPoint>) -> std::io::Result<()> {
     Ok(())
 }
 
-fn record()
-{
+fn record() {
     thread::spawn(|| {
         if let Err(error) = listen(callback) {
             println!("Error: {:?}", error)
@@ -92,19 +85,21 @@ fn record()
         let now = SystemTime::now();
         let now: DateTime<Utc> = now.into();
         //let now = now.to_rfc3339();
-        let timed_pos = TimedPoint{x: pos.0 as f64 / resolution.0 as f64, y: pos.1 as f64 / resolution.1 as f64, time: now};
+        let timed_pos = TimedPoint {
+            x: pos.0 as f64 / resolution.0 as f64,
+            y: pos.1 as f64 / resolution.1 as f64,
+            time: now,
+        };
         println!("{}", &timed_pos);
         vec.push(timed_pos);
         thread::sleep(Duration::from_millis(100));
 
         counter = counter + 1;
-        if counter >= 4
-        {
+        if counter >= 4 {
             println!("{}", vec.len());
-            match write_points(&vec)
-            {
+            match write_points(&vec) {
                 Ok(v) => println!("ok: {:?}", v),
-                Err(e) => println!("err: {}", e)
+                Err(e) => println!("err: {}", e),
             }
 
             std::process::exit(0);
