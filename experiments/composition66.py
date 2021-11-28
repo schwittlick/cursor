37641893c2432367c693bdd401173fa1f8846b75
Marcel Schwittlick <info@schwittlick.net>
2021-11-28 19:00:51 +0100

    remove merge leftover and remove global hpgl speed

--- experiments/composition66.py
+++ experiments/composition66.py
@@ -1,62 +1,4 @@
 from cursor.path import Path, PathCollection
-<<<<<<< HEAD
-from cursor.renderer import RealtimeRenderer
-from cursor import device
-
-import math
-
-
-def plain_spiral(thetamax, yoffset, yadd, xextra):
-    pp = Path(layer="round1")
-    theta = 0
-    ypos = 0
-    xpos = 0
-    r = 30
-    while theta < thetamax: #* 20:
-        y = r * math.cos(theta) + xpos
-        x = r * math.sin(theta) + ypos
-        pp.add(x, y + yoffset, 0)
-        theta += 0.08
-        ypos += yadd
-        xpos += xextra
-
-    return pp
-
-
-if __name__ == "__main__":
-    coll = PathCollection()
-
-    xplus = 0.25
-    yplus = 0.25
-
-    p1 = plain_spiral(255, 0, xplus, 0)
-    p2 = plain_spiral(255, 400, xplus, 0)
-    p3 = plain_spiral(155, 0, 0, yplus)
-    p4 = plain_spiral(155, 0, xplus, yplus)
-
-    num = 'ad_strauss'
-
-    # reversed_path = pp.reversed()
-    # reversed_path.layer = "round2"
-
-    coll.add(p1)
-    coll.add(p2)
-    coll.add(p3)
-    coll.add(p4)
-    # coll.add(reversed_path)
-
-    #r = RealtimeRenderer()
-    #r.render(coll)
-
-    device.SimpleExportWrapper().ex(
-        coll,
-        device.PlotterType.ROLAND_DPX3300,
-        device.PaperSize.LANDSCAPE_A1,
-        50,
-        "composition66",
-        num,
-    )
-=======
 from cursor import device
 import matplotlib
 
@@ -226,4 +168,3 @@ if __name__ == "__main__":
     #    "composition66",
     #    num,
     # )
->>>>>>> 679d9204f4692f549e957e8a9b229537258b2053
