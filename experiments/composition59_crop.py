f235188095f7c90cc7b227d63943d165bb8cb673
Marcel Schwittlick <info@schwittlick.net>
2022-02-12 19:47:06 +0100

    adds sketch for cropping pipeline

--- experiments/composition59_crop.py
+++ experiments/composition59_crop.py
@@ -7,8 +7,71 @@ data = composition59_data.data_map
 
 import random
 import cv2
+import pathlib
+
+oriImage = None
+cropping = False
+x_start, y_start, x_end, y_end = 0, 0, 0, 0
+filename = None
+offset = 100
+id = 1111
+
+
+def mouse_crop(event, x, y, flags, param):
+    # grab references to the global variables
+    global x_start, y_start, x_end, y_end, cropping
+    # if the left mouse button was DOWN, start RECORDING
+    # (x, y) coordinates and indicate that cropping is being
+    if event == cv2.EVENT_LBUTTONDOWN:
+        x_start, y_start, x_end, y_end = x, y, x, y
+        cropping = True
+    # Mouse is Moving
+    elif event == cv2.EVENT_MOUSEMOVE:
+        if cropping == True:
+            x_end, y_end = x, y
+    # if the left mouse button was released
+    elif event == cv2.EVENT_LBUTTONUP:
+        # record the ending (x, y) coordinates
+        x_end, y_end = x, y
+        cropping = False  # cropping is finished
+        refPoint = [(x_start, y_start), (x_end, y_end)]
+        if len(refPoint) == 2:  # when two points were found
+            a, b, c, d = x_start * 3, y_start * 3, x_end * 3, y_end * 3
+            roi = oriImage[b:d, a:c]
+            cv2.imshow("Cropped", roi)
+            roi = oriImage[b - offset : d + offset, a - offset : c + offset]
+            p = pathlib.Path(filename)
+            t = f"Z:\\144\\cropped\\{id}.jpg"
+            cv2.imwrite(t, roi)
+
+            cropping = False
+
+
+def crop_interactive(fn, _id):
+    global filename
+    filename = fn
+    global id
+    id = _id
+
+    cv2.namedWindow("image")
+    cv2.setMouseCallback("image", mouse_crop)
+
+    global oriImage
+    oriImage = cv2.imread(fn, cv2.IMREAD_UNCHANGED)
+
+    while True:
+        i = cv2.resize(
+            oriImage, (int(oriImage.shape[1] / 3.0), int(oriImage.shape[0] / 3.0))
+        )
+        cv2.imshow("image", i)
+        if cv2.waitKey() is ord("q"):
+            break
+
+    cv2.destroyAllWindows()
+
 
 def crop(fn):
+
     im = cv2.imread(fn, cv2.IMREAD_UNCHANGED)
     img = cv2.pyrDown(im)
 
@@ -36,11 +99,12 @@ def crop(fn):
 
 
 if __name__ == "__main__":
-
-    selected = composition59_data.data_map[composition59_data.yellow][composition59_data.orange]
-    crop(selected["file"])
+    selected = composition59_data.data_map[composition59_data.yellow][
+        composition59_data.orange
+    ]
+    crop_interactive(selected["file"], selected["objktid"])
     for k1, v1 in composition59_data.data_map.items():
         for k2, v2 in v1.items():
-            print(k1 + " " +k2)
+            print(k1 + " " + k2)
             if "1111" not in v2["file"]:
-                crop(v2["file"])
+                crop_interactive(v2["file"], f"{k1}-{k2}")
