032dd79571f34f93d91c57430ebd4f89f3f2dffa
Marcel Schwittlick <info@schwittlick.net>
2022-02-22 12:09:33 +0100

    some test fixing

--- cursor/tests/test_path.py
+++ cursor/tests/test_path.py
@@ -400,16 +400,16 @@ def test_path_intersection3():
 
 def test_angles():
     p = Path()
-    p.add(0, 0)
-    p.add(0, 1)
-    p.add(1, 1)
-    p.add(1, 0)
-    p.add(0.5, 0)
+    p.add(0.0, 0.0)
+    p.add(0.0, 1.0)
+    p.add(1.0, 1.0)
+    p.add(1.0, 0.0)
+    p.add(0.5, 0.0)
 
     changes = p.direction_changes()
     assert changes[0] == 0.0
-    assert changes[1] == 45.00000000000001  # wat
-    assert changes[2] == 45.00000000000001
+    assert round(changes[1], 2) == 45.0  # wat
+    assert round(changes[2], 2) == 45.0
     assert changes[3] == 0.0
 
 
