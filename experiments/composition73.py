baf98031b7765e1a4677248e1e62960392779647
Marcel Schwittlick <info@schwittlick.net>
2022-03-01 16:39:53 +0100

    satisfy linters

--- experiments/composition73.py
+++ experiments/composition73.py
@@ -11,11 +11,20 @@ if __name__ == "__main__":
     _loader = loader.Loader(directory=recordings, limit_files=1)
     all_paths = _loader.all_paths()
 
-    r = all_paths.random()
-    r.clean()
-    pc.add(r)
-    for i in range(100):
-        pc.add(pc[0].offset(-0.002 * i))
+    r1 = all_paths[1]
+    r2 = r1.copy()
+
+    r1.smooth(5, 1)
+
+    pc.add(r1)
+    # pc.add(r2)
+
+    v = 0.05
+
+    pc.add(pc[0].offset(v))
+    pc.add(pc[0].offset(-v))
+    # pc.add(pc[1].offset(v))
+    # pc.add(pc[1].offset(-v))
 
     device.SimpleExportWrapper().ex(
         pc,
