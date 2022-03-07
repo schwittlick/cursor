a4d696c78a842faee53557dafcdea9b9dcf96108
Marcel Schwittlick <info@schwittlick.net>
2022-03-07 17:05:15 +0100

    refactoring

--- cursor/misc.py
+++ cursor/misc.py
@@ -1,6 +1,7 @@
 from cursor import data
 from cursor import path
-from cursor import renderer
+from cursor.renderer import SvgRenderer
+from cursor.renderer import JpegRenderer
 
 import time
 import numpy as np
@@ -97,13 +98,13 @@ def map(value, inputMin, inputMax, outputMin, outputMax, clamp):
 
 def save_wrapper(pc, projname, fname):
     jpeg_folder = data.DataDirHandler().jpg(projname)
-    jpeg_renderer = renderer.JpegRenderer(jpeg_folder)
+    jpeg_renderer = JpegRenderer(jpeg_folder)
 
     jpeg_renderer.render(pc, scale=1.0)
     jpeg_renderer.save(fname)
 
     svg_folder = data.DataDirHandler().svg(projname)
-    svg_renderer = renderer.SvgRenderer(svg_folder)
+    svg_renderer = SvgRenderer(svg_folder)
 
     svg_renderer.render(pc)
     svg_renderer.save(fname)
@@ -111,7 +112,7 @@ def save_wrapper(pc, projname, fname):
 
 def save_wrapper_jpeg(pc, projname, fname, scale=4.0, thickness=3):
     folder = data.DataDirHandler().jpg(projname)
-    jpeg_renderer = renderer.JpegRenderer(folder)
+    jpeg_renderer = JpegRenderer(folder)
 
     jpeg_renderer.render(pc, scale=scale, thickness=thickness)
     jpeg_renderer.save(fname)
