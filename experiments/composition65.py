661e316e5d6b436d699a9ae46f66318bc85e7bd3
Marcel Schwittlick <info@schwittlick.net>
2022-03-29 15:14:14 +0200

    working on some experiments

--- experiments/composition65.py
+++ experiments/composition65.py
@@ -1,38 +1,22 @@
 from cursor import loader
 from cursor import data
 from cursor import device
-from cursor import path
 from cursor import filter
 
 
 if __name__ == "__main__":
     recordings = data.DataDirHandler().recordings()
-    _loader = loader.Loader(directory=recordings, limit_files=None)
-    all_paths = _loader.all_paths()
+    l = loader.Loader(directory=recordings)
+    all_paths = l.all_paths()
 
     distance_filter = filter.DistanceFilter(0.01)
     all_paths.filter(distance_filter)
 
-    all_paths.limit()
-
-    pc = path.PathCollection()
-
-    counter = 0
-    for p in all_paths:
-        # p.layer = counter
-        pc.add(p)
-
-        counter += 1
-        counter = counter % 6
-
-    # pc.reorder_quadrants(10, 10)
-
     device.SimpleExportWrapper().ex(
-        pc,
+        all_paths,
         device.PlotterType.ROLAND_DXY1200,
         device.PaperSize.LANDSCAPE_A3,
         15,
         "composition65",
-        # f"c65_{pc.hash()}",
-        "c65_001distance_unordered",
+        f"c65_{all_paths.hash()}",
     )
