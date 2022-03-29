dc1b6e35244bcbeb2b9325520061f705f109dddf
Marcel Schwittlick <info@schwittlick.net>
2022-03-29 15:14:14 +0200

    working on some experiments

--- experiments/composition62.py
+++ experiments/composition62.py
@@ -7,21 +7,22 @@ from cursor import filter
 
 if __name__ == "__main__":
     p = data.DataDirHandler().recordings()
-    files = ["1645457267.665355_compressed"]
+    files = ["1644923397.358839_compressed"]
     ll = loader.Loader(directory=p, limit_files=files)
     colls = ll.all_paths()
 
+    colls.limit()
     c = path.PathCollection()
 
-    layer = 0
-    for p in colls:
-        add = True
-        for pos in p:
-            if pos.x > 1.1:
-                add = False
-        # p.layer = str(layer)
-        if add:
-            c.add(p)
+    #layer = 0
+    #for p in colls:
+    #    add = True
+    #    for pos in p:
+    #        if pos.x > 1.1:
+    #            add = False
+    #    # p.layer = str(layer)
+    #    if add:
+    #        c.add(p)
     # layer += 1
 
     # c.fit(device.Paper.sizes[device.PaperSize.LANDSCAPE_A1], padding_mm=20)
@@ -30,10 +31,11 @@ if __name__ == "__main__":
     # f = filter.BoundingBoxFilter(bb)
     # c.filter(f)
 
+
     # misc.save_wrapper_jpeg(c, "composition62", "composition62_together", 4.0, 1.0)
 
     device.SimpleExportWrapper().ex(
-        c,
+        colls,
         device.PlotterType.ROLAND_DPX3300,
         device.PaperSize.LANDSCAPE_A1,
         20,
--- experiments/composition69.py
+++ experiments/composition69.py
@@ -2,14 +2,43 @@ from cursor import device
 from cursor import path
 from cursor import misc
 
-if __name__ == "__main__":
+def four_blocks():
     pc = path.PathCollection()
-    count = 390 * 4
+
+    for block in range(4):
+        count = 390
+        for v in range(count):
+            pa = path.Path(pen_velocity=int(misc.map(v, 0, count, 45, 1, True)))
+            v += block * count
+            pa.add(v, 0)
+            pa.add(v, 10)
+            pa.pen_select = block + 1
+            pc.add(pa)
+
+    return pc
+
+
+def alternating():
+    pc = path.PathCollection()
+
+    count = 390
     for v in range(count):
-        pa = path.Path(pen_velocity=int(misc.map(v, 0, count, 45, 1, True)))
+        if v % 2 == 0:
+            velo = 1
+        else:
+            velo = 45
+        pa = path.Path(pen_velocity=int(velo))
         pa.add(v, 0)
         pa.add(v, 10)
+        #pa.pen_select = (count % 2) + 1
         pc.add(pa)
+
+    return pc
+
+if __name__ == "__main__":
+
+    #pc = four_blocks()
+    pc = alternating()
 
     device.SimpleExportWrapper().ex(
         pc,
@@ -17,5 +46,5 @@ if __name__ == "__main__":
         device.PaperSize.LANDSCAPE_A1,
         30,
         "composition69",
-        f"c69_0.5mm_{pc.hash()}",
+        f"c69_alternating_{pc.hash()}",
     )
