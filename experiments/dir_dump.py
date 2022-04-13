fef340a74fac12e1d02129bb23adad3d69d69ff7
Marcel Schwittlick <info@schwittlick.net>
2022-04-13 18:13:26 +0200

    removes source

--- experiments/dir_dump.py
+++
@@ -1,9 +0,0 @@
-import pathlib
-
-if __name__ == "__main__":
-    with open("julis_movie_list.txt", "w") as file:
-        path = pathlib.Path("D:\\movies_julius_marcel\\")
-        for p in path.rglob("*"):
-
-            print(p.as_posix())
-            file.write(p.as_posix() + "\n")
--- experiments/ocr.py
+++
@@ -1,8 +0,0 @@
-from PIL import Image
-import pytesseract
-
-if __name__ == "__main__":
-    im1 = "Z:\\Dropbox\\0_MARCELSCHWITTLICK\\2020_DRAWING_PHOTOS\\c71\\20220304_crop area 1.jpg"
-    print(pytesseract.image_to_string(Image.open(im1)))
-
-    print(pytesseract.image_to_string(Image.open(im1), lang='deu'))
