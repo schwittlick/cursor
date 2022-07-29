b079cd851fa060558a298e16dcceffc5d6a5f551
Marcel Schwittlick <info@schwittlick.net>
2022-07-29 17:43:01 +0200

    extend sender tool a bit

---
+++ tools/baud_detector.py
@@ -0,0 +1,61 @@
+import serial
+import sys
+import codecs 
+import time 
+
+if __name__ == "__main__":
+    bauds = [
+        #  75,
+        #  110,
+        #  150,
+        #  200,
+        #  300,
+        #  600,
+        #  1200,
+        #    2400,
+        #  4800,
+        9600,
+        #  19200,
+        #  38400,
+        #  57600,
+        #  74880,
+        #  115200,
+    ]
+    port = "COM3"
+    response = ""
+    for baud in bauds:
+        s = serial.Serial(
+            port,
+            baud,
+            timeout=1,
+            parity=serial.PARITY_NONE,
+            bytesize=serial.EIGHTBITS,
+            stopbits=serial.STOPBITS_ONE,
+        )
+        s.write(b"OH;")
+        response = s.readline()
+        print(response)
+        s.write(b"IN;SP1;PU100,100;")
+        time.sleep(1)
+        response2 = s.readline()
+        print(f"response2: {response2}")
+        s.close()
+        if response:
+            print(f"{response}")
+            #string = response.decode('hex')
+            string = response[1:]
+            #r1 = string[:1] + string[2:]
+            r2 = string.hex()
+            print(string)
+            print(r2)
+            bytes_object = bytes.fromhex(r2)
+            ascii_string = bytes_object.decode("latin-1")
+            print(ascii_string)
+            #string.remove(4)
+            print(string)
+            s = codecs.decode(r2, 'hex')
+            print(s)
+            #string = bytearray.fromhex(string.decode('utf-8')).decode()
+            #print(string)
+            #print(response.hex().decode("hex"))
+            sys.exit(0)
--- tools/serial_sender.py
+++ tools/serial_sender.py
@@ -17,55 +17,79 @@
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 #  python serial_sender.py /dev/ttyUSB0 1200 0 path/to/fi.le
-
+import typing
 from argparse import ArgumentParser
 from serial import Serial
 from time import sleep
 
 
+class Sender:
+    def __init__(self, port: str, baud: int):
+        self.__serial = Serial(port=port, baudrate=baud, timeout=2)
+
+    @staticmethod
+    def show_progress(pos, total: int, length: int = 100):
+        fill = length * pos // total
+        print(
+            "\rProgress: ["
+            + fill * "\u2588"
+            + (length - fill) * "\u2591"
+            + "] "
+            + str(pos)
+            + " of "
+            + str(total)
+            + " bytes sent",
+            end="\r",
+        )
+        if pos == total:
+            print()
+
+    def does_feedback(self) -> typing.Tuple[int, bool]:
+        self.__serial.write(b"\x1B.B")
+        b = b""
+        n = 0
+
+        read_some = False
+        while b != b"\r":
+            if len(b) > 0:
+                read_some = True
+                n = n * 10 + b[0] - 48
+            b = self.__serial.read()
+
+        return n, read_some
+
+    def send(self, data: str):
+        pass
+
+
 def read_code(path):
-    code = ''
-    with open(path, 'r') as f:
+    code = ""
+    with open(path, "r") as f:
         code = f.read()
     return code
 
 
-def check_avail(serial):
-    serial.write(b'\x1B.B')
-    b = b''
-    n = 0
-    while b != b'\r':
-        if len(b) > 0:
-            n = n * 10 + b[0] - 48
-        b = serial.read()
-    return n
-
-
-def show_progress(pos, total, length=100):
-    fill = length * pos // total
-    print('\rProgress: [' + fill * '\u2588' + (length - fill) * '\u2591' + '] ' + str(pos) + ' of ' + str(
-        total) + ' bytes sent', end='\r')
-    if pos == total:
-        print()
-
-
 def main():
     parser = ArgumentParser()
-    parser.add_argument('port')
-    parser.add_argument('baud', type=int)
-    parser.add_argument('hpgl', type=int)
-    parser.add_argument('file')
+    parser.add_argument("port")
+    parser.add_argument("baud", type=int)
+    parser.add_argument("hpgl", type=int)
+    parser.add_argument("file")
     args = parser.parse_args()
+
+    sender = Sender(args.port, args.baud)
+    feedback = sender.does_feedback()
+
     print(bool(args.hpgl))
     serial = Serial(port=args.port, baudrate=args.baud, timeout=0)
     code = read_code(args.file)
     pos = 0
 
-    show_progress(pos, len(code))
+    Sender.show_progress(pos, len(code))
     while pos < len(code):
         avail = 512
-        if bool(args.hpgl):
-            avail = check_avail(serial)
+        if feedback:
+            avail, success = sender.does_feedback()
         if avail < 512:
             sleep(0.01)
             continue
@@ -74,11 +98,11 @@ def main():
         if len(code) - pos < avail:
             end = len(code)
 
-        serial.write(code[pos:end].encode('utf-8'))
+        serial.write(code[pos:end].encode("utf-8"))
         pos = end
 
-        show_progress(pos, len(code))
+        Sender.show_progress(pos, len(code))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
