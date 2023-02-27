f6575e5e0a274f88fcec4cf1925435d2f97b1ced
Marcel Schwittlick <info@schwittlick.net>
2023-02-27 17:50:25 +0100

    adds launchpad to connect

---
+++ tools/octet/launchpad_wrapper.py
@@ -0,0 +1,47 @@
+import sys
+import time
+
+try:
+    import launchpad_py as launchpad
+except ImportError as e:
+    print(e)
+    try:
+        import launchpad
+    except ImportError as e:
+        print(e)
+        sys.exit("error loading launchpad.py")
+
+
+class NovationLaunchpad:
+    def __init__(self):
+        self.mode = None
+        self.lp = None
+        if launchpad.Launchpad().Check(0):
+            self.lp = launchpad.Launchpad()
+            if self.lp.Open(0):
+                print("Launchpad Mk1/S/Mini")
+                self.mode = "Mk1"
+
+        if self.mode is None:
+            print("Did not find any Launchpads, meh...")
+            return
+
+    def poll(self):
+        buts = self.lp.ButtonStateXY()
+        if buts != []:
+            brightness = 2 if buts[2] else 0
+            self.lp.LedCtrlXY(buts[0], buts[1], brightness, brightness)
+        return buts
+
+
+def main():
+    lp = NovationLaunchpad()
+
+    while True:
+        state = lp.poll()
+
+        time.sleep(0.001)
+
+
+if __name__ == '__main__':
+    main()
