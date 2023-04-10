a67d26ea8a09fd93de49e109bc06db01c332b1f2
Marcel Schwittlick <info@schwittlick.net>
2023-04-10 13:04:05 +0200

    refactoring

--- tools/raffle/raffle_assignment.py
+++ tools/raffle/raffle_assignment.py
@@ -4,14 +4,11 @@ import random
 import requests
 
 
-def generate_holders_query(contract: str, token: int):
+def generate_holders_query(contract: str, token: int) -> str:
     return f''' query LatestEvents {{
       tokens_by_pk(fa2_address: "{contract}", token_id: "{token}") {{
         holdings(where: {{amount: {{_gt: "0"}}}}) {{
           holder_address
-          holder_profile {{
-            alias
-          }}
           amount
         }}
       }}
@@ -19,40 +16,34 @@ def generate_holders_query(contract: str, token: int):
     '''
 
 
-def get_data(query: str):
+def get_data(query: str) -> dict:
     url = 'https://api.teztok.com/v1/graphql'
     r = requests.post(url, json={'query': query})
     return json.loads(r.text)
 
 
-def parse(data: dict):
+def parse(data: dict) -> list[str]:
     holder_addresses = []
 
     holdings = data["data"]["tokens_by_pk"]["holdings"]
     for holding in holdings:
         address = holding['holder_address']
-        profile = holding['holder_profile']
-        if profile:
-            address = f"{address} {profile['alias']}"
         amount = holding['amount']
         holder_addresses.extend([address for _ in range(amount)])
 
     return holder_addresses
 
 
-def do_raffle():
+def do_raffle() -> None:
     contract = "KT1FRjrFbRbAcJYuXiwJxmQC5sYpHgXbLQ4S"
     query = generate_holders_query(contract, 0)
     data = get_data(query)
     holders = parse(data)
 
-    token_ids = list(range(1, len(holders) + 1))
+    random.shuffle(holders)
 
-    for id in token_ids:
-        random_holder_idx = random.randrange(len(holders))
-        random_holder = holders.pop(random_holder_idx)
-
-        print(f"{id} -> {random_holder}")
+    for idx, holder in enumerate(holders):
+        print(f"{holder} 🫴 token id {idx + 1}")
 
 
 if __name__ == '__main__':
