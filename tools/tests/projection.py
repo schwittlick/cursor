a493d4aaa7af126528eabd2d703d326e5618f5d3
Marcel Schwittlick <info@schwittlick.net>
2023-06-27 10:16:02 +0200

    refactors projection

--- tools/tests/projection.py
+++ tools/tests/projection.py
@@ -9,6 +9,10 @@ from cursor.renderer import RealtimeRenderer
 from cursor.timer import Timer
 from tools.spline_experiment import transform_path
 
+thefont = HersheyFonts()
+thefont.load_default_font()
+thefont.normalize_rendering(100)
+
 
 def project_point_to_plane(point, plane_point, plane_normal):
     plane_normal = plane_normal / np.linalg.norm(plane_normal)  # Normalize the plane normal vector
@@ -44,6 +48,13 @@ def project(point_list: list[list[list[float]]], plane_point: np.array = np.arra
 
 
 def on_mouse(rr: RealtimeRenderer, x, y, dx, dy):
+    timer = Timer()
+
+    temp_points = []
+
+    for (x1, y1), (x2, y2) in thefont.lines_for_text('HEYHEY'):
+        temp_points.append([[x1, -y1, 0], [x2, -y2, 0]])
+
     print(rr)
     print(x, y, dx, dy)
 
@@ -66,36 +77,12 @@ def on_mouse(rr: RealtimeRenderer, x, y, dx, dy):
     rr.clear_list()
     rr.add_collection(c_final, line_width=2, color=arcade.color.GRAY)
 
+    timer.print_elapsed("")
+
 
 if __name__ == "__main__":
-    temp_points = []
-    thefont = HersheyFonts()
-    thefont.load_default_font()
-    thefont.normalize_rendering(100)
-    for (x1, y1), (x2, y2) in thefont.lines_for_text('III...III'):
-        temp_points.append([[x1, -y1, 0], [x2, -y2, 0]])
-
-    plane_point = np.array([0, 0, 0])
-    plane_normal = np.array([0, 0, 1])
-
-    # Choose basis vectors for the plane (they should be orthogonal)
-    basis1 = np.array([1, 0, 0])
-    basis2 = np.array([0, 1, 0])
-
-    timer = Timer()
-    c = project(temp_points, plane_point, plane_normal, basis1, basis2)
-
-    c_final = Collection()
-
-    bb = c.bb()
-    for p in c:
-        c_final.add(transform_path(p, bb, ((0, 0), (400, 400))))
-
-    timer.print_elapsed("this took ")
-
     rr = RealtimeRenderer(400, 400, "projection")
     rr.set_bg_color(arcade.color.WHITE)
-    rr.add_collection(c_final, line_width=2, color=arcade.color.GRAY)
     rr.set_on_mouse_cb(on_mouse)
 
     rr.run()
