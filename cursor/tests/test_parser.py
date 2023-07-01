6f9eaeb60975d9d25c4fb4d6856af82c8ff79157
Marcel Schwittlick <info@schwittlick.net>
2023-07-01 23:16:07 +0200

    implements PD, PU in hpgl parser

--- cursor/tests/test_parser.py
+++ cursor/tests/test_parser.py
@@ -4,7 +4,7 @@ from cursor.parser import HPGLParser
 from cursor.path import Path
 
 
-def test_hpgl_parser():
+def test_hpgl_parser_LB():
     file = DataDirHandler().test_data_file("file_to_parse.hpgl")
     parser = HPGLParser(file)
     paths = parser.parse()
@@ -12,5 +12,16 @@ def test_hpgl_parser():
     collection = Collection()
     collection.add(Path.from_tuple_list([(100.25, 2.0), (100.875, 2.0)]))
     collection.add(Path.from_tuple_list([(100.25, 1.375), (100.625, 1.0), (100.625, 2.0)]))
+
+    assert paths == collection
+
+
+def test_hpgl_parser_PA():
+    file = DataDirHandler().test_data_file("hpgl_pa_pd_pu.hpgl")
+    parser = HPGLParser(file)
+    paths = parser.parse()
+
+    collection = Collection()
+    collection.add(Path.from_tuple_list([(100, 0), (100, 100)]))
 
     assert paths == collection
