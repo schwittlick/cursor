2ec04d494c88719ecbbe5572b3e535d700ec52aa
Marcel Schwittlick <info@schwittlick.net>
2023-07-22 14:00:51 +0200

    shortens path tests

--- cursor/tests/test_path.py
+++ cursor/tests/test_path.py
@@ -57,9 +57,7 @@ def test_path_copy():
 
 
 def test_path_start_end():
-    p = Path()
-    p.add(0, 0, 10000)
-    p.add(10, 11, 10000)
+    p = Path.from_tuple_list([(0, 0), (10, 11)])
 
     start = p.start_pos()
     assert start.x == 0
@@ -71,10 +69,7 @@ def test_path_start_end():
 
 
 def test_path_scale():
-    p = Path()
-    p.add(3, 5)
-    p.add(5, 9)
-    p.add(-3, -10)
+    p = Path.from_tuple_list([(3, 5), (5, 9), (-3, -10)])
 
     p.scale(2, 5)
 
@@ -87,9 +82,7 @@ def test_path_scale():
 
 
 def test_path_translate():
-    p = Path()
-    p.add(3, 5)
-    p.add(5, 9)
+    p = Path.from_tuple_list([(3, 5), (5, 9)])
     p.translate(2, 5)
 
     assert p[0].x == 5
@@ -99,11 +92,9 @@ def test_path_translate():
 
 
 def test_path_bb():
-    p = Path()
-    p.add(0, 0, 10000)
-    p.add(140, 11, 10000)
-    p.add(23, 141, 10000)
-    p.add(141, 4511, 10000)
+    p = Path.from_tuple_list(
+        [(0, 0), (140, 11),
+         (23, 141), (141, 4511)])
 
     bb = p.bb()
     assert bb.x == 0
@@ -113,11 +104,9 @@ def test_path_bb():
 
 
 def test_path_bb2():
-    p = Path()
-    p.add(100, 100, 0)
-    p.add(200, 100, 0)
-    p.add(100, 200, 0)
-    p.add(200, 200, 0)
+    p = Path.from_tuple_list(
+        [(100, 100), (200, 100),
+         (100, 200), (200, 200)])
 
     bb = p.bb()
     assert bb.x == 100
@@ -127,11 +116,8 @@ def test_path_bb2():
 
 
 def test_path_morph():
-    p = Path()
-
-    p.add(19, 34, 10000)
-    p.add(10, 10, 10000)
-    p.add(600, 10, 10001)
+    p = Path.from_tuple_list(
+        [(19, 34), (10, 10), (600, 10)])
 
     pm = p.morph((0, 0), (10, 100))
 
@@ -144,11 +130,7 @@ def test_path_morph():
 
 
 def test_path_morph2():
-    p = Path()
-
-    p.add(19, 34, 10000)
-    p.add(10, 10, 10000)
-    p.add(600, 10, 10001)
+    p = Path.from_tuple_list([(19, 34), (10, 10), (600, 10)])
 
     pm = p.morph((10, 100), (0, 0))
 
@@ -161,13 +143,8 @@ def test_path_morph2():
 
 
 def test_path_interp():
-    p = Path()
-    p.add(0, 0, 10000)
-    p.add(100, 0, 10001)
-
-    p1 = Path()
-    p1.add(0, 100, 10000)
-    p1.add(100, 100, 10001)
+    p = Path.from_tuple_list([(0, 0), (100, 0)])
+    p1 = Path.from_tuple_list([(0, 100), (100, 100)])
 
     interped = p.interp(p1, 0.5)
 
@@ -193,20 +170,13 @@ def test_sort_path():
 
 
 def test_entropy():
-    p1 = Path()
-    p1.add(100, 34, 10040)
-    p1.add(200, 10, 10000)
-    p1.add(100, 10, 10001)
-    p1.add(200, 10, 10001)
-    p1.add(100, 10, 10001)
-    p1.add(200, 10, 10001)
+    p1 = Path.from_tuple_list(
+        [(100, 34), (200, 10), (100, 10),
+         (200, 10), (100, 10), (200, 20)])
 
-    p2 = Path()
-    p2.add(200, 10, 10000)
-    p2.add(200, 10, 10001)
-    p2.add(200, 10, 10001)
-    p2.add(200, 10, 10001)
-    p2.add(200, 10, 10001)
+    p2 = Path.from_tuple_list(
+        [(200, 10), (200, 10), (200, 10),
+         (200, 10), (200, 10)])
 
     sx1 = p1.entropy_x
     sx2 = p2.entropy_x
@@ -218,11 +188,8 @@ def test_entropy():
 
 
 def test_entropy_by_position1():
-    p1 = Path()
-    p1.add(10, 10)
-    p1.add(10, 10)
-    p1.add(10, 10)
-    p1.add(10, 10)
+    p1 = Path.from_tuple_list(
+        [(10, 10), (10, 10), (10, 10), (10, 10)])
 
     sx1 = p1.entropy_x
     sy1 = p1.entropy_y
@@ -232,30 +199,24 @@ def test_entropy_by_position1():
 
 
 def test_entropy_by_position2():
-    p1 = Path()
-    p1.add(10, 10)
-    p1.add(11, 10)
-    p1.add(12, 10)
-    p1.add(13, 10)
+    p1 = Path.from_tuple_list(
+        [(10, 10), (11, 10), (12, 10), (13, 10)])
 
     sx1 = p1.entropy_x
     sy1 = p1.entropy_y
 
-    assert sx1 == 1.3862943611198906
-    assert sy1 == 0.0
+    assert math.isclose(sx1, 1.386294361119)
+    assert math.isclose(sy1, 0.0)
 
 
 def test_variation():
-    p1 = Path()
-    p1.add(10, 10)
-    p1.add(11, 10)
-    p1.add(12, 10)
-    p1.add(13, 10)
+    p1 = Path.from_tuple_list(
+        [(10, 10), (11, 10), (12, 10), (13, 10)])
 
     sx = p1.variation_x
     sy = p1.variation_y
 
-    assert math.isclose(sx, 0.11226038684659179)
+    assert math.isclose(sx, 0.11226038684)
     assert math.isclose(sy, 0.0)
 
 
@@ -284,17 +245,13 @@ def test_simplify_random():
 def test_entropy_by_position3():
     # lower entropy value
     # because repeated
-    p1 = Path()
-    p1.add(10, 10)
-    p1.add(11, 10)
-    p1.add(10, 10)
-    p1.add(11, 10)
+    p1 = Path.from_tuple_list([(10, 10), (11, 10), (10, 10), (11, 10)])
 
     sx1 = p1.entropy_x
     sy1 = p1.entropy_y
 
-    assert sx1 == 0.6931471805599453
-    assert sy1 == 0.0
+    assert math.isclose(sx1, 0.693147180559)
+    assert math.isclose(sy1, 0)
 
 
 def test_entropy_by_position4():
@@ -311,16 +268,9 @@ def test_entropy_by_position4():
 
 
 def test_path_clean():
-    p = Path()
-
-    p.add(5, 5111, 10000)
-    p.add(5, 5111, 10000)
-    p.add(5, 5112, 10000)
-    p.add(5, 5112, 10000)
-    p.add(5, 5112, 10000)
-    p.add(6, 5112, 10000)
-    p.add(6, 5112, 10000)
-    p.add(6, 5112, 10000)
+    p = Path.from_tuple_list(
+        [(1, 1), (1, 1), (1, 2), (1, 2),
+         (1, 2), (2, 2), (2, 2), (2, 2)])
 
     assert len(p) == 8
 
@@ -330,15 +280,9 @@ def test_path_clean():
 
 
 def test_path_limit():
-    p = Path()
-
-    p.add(0.9, 0.9, 0)
-    p.add(0.9, 1.0, 0)
-    p.add(0.9, 1.1, 0)
-    p.add(0.1, 0.8, 0)
-    p.add(-0.1, 0.8, 0)
-    p.add(0.0, 0.0, 0)
-    p.add(1.0, 1.0, 0)
+    p = Path.from_tuple_list(
+        [(0.9, 0.9), (0.9, 1.0), (0.9, 1.1), (0.1, 0.8),
+         (-0.1, 0.8), (0.0, 0.0), (1.0, 1.0)])
 
     p.limit()
 
@@ -346,15 +290,9 @@ def test_path_limit():
 
 
 def test_path_offset_shapely():
-    p = Path()
-
-    p.add(0.9, 0.9, 0)
-    p.add(0.9, 1.0, 0)
-    p.add(0.9, 1.1, 0)
-    p.add(0.1, 0.8, 0)
-    p.add(-0.1, 0.8, 0)
-    p.add(0.0, 0.0, 0)
-    p.add(1.0, 1.0, 0)
+    p = Path.from_tuple_list(
+        [(0.9, 0.9), (0.9, 1.0), (0.9, 1.1), (0.1, 0.8),
+         (-0.1, 0.8), (0.0, 0.0), (1.0, 1.0)])
     p.scale(10, 10)
 
     new = p.parallel_offset(1)
@@ -364,11 +302,7 @@ def test_path_offset_shapely():
 
 
 def test_path_nearest_points():
-    p = Path()
-
-    p.add(0.0, 0.0, 0)
-    p.add(1.0, 1.1, 0)
-    p.add(2.0, 0.0, 0)
+    p = Path.from_tuple_list([(0, 0), (1, 1.1), (2, 0)])
 
     new = p.nearest_points(Position(1.0, 2.0))
 
@@ -377,11 +311,7 @@ def test_path_nearest_points():
 
 
 def test_path_dilate():
-    p = Path()
-
-    p.add(0.0, 0.0, 0)
-    p.add(1.0, 1.1, 0)
-    p.add(2.0, 0.0, 0)
+    p = Path.from_tuple_list([(0, 0), (1, 1.1), (2, 0)])
 
     new = p.dilate_erode(2.0)
 
@@ -392,32 +322,18 @@ def test_path_reverse():
     """
     testing in-place reverse() and reversed()
     """
-    p1 = Path()
-    p1.add(5, 5111, 10000)
-    p1.add(5, 5112, 10000)
-    p1.add(6, 5112, 10000)
-
-    p_test = Path()
-    p_test.add(5, 5111, 10000)
-    p_test.add(5, 5112, 10000)
-    p_test.add(6, 5112, 10000)
+    p1 = Path.from_tuple_list([(1, 1), (2, 2), (3, 3)])
+    p_test = Path.from_tuple_list([(1, 1), (2, 2), (3, 3)])
 
     assert p1 == p_test
 
     p1.reverse()
 
-    p_reversed = Path()
-    p_reversed.add(6, 5112, 10000)
-    p_reversed.add(5, 5112, 10000)
-    p_reversed.add(5, 5111, 10000)
+    p_reversed = Path.from_tuple_list([(3, 3), (2, 2), (1, 1)])
 
     assert p1 == p_reversed
 
-    p2 = Path()
-
-    p2.add(5, 5111, 10000)
-    p2.add(5, 5112, 10000)
-    p2.add(6, 5112, 10000)
+    p2 = Path.from_tuple_list([(1, 1), (2, 2), (3, 3)])
 
     p3 = p2.reversed()
 
@@ -439,13 +355,8 @@ def test_path_layer():
 
 
 def test_path_intersection1():
-    p = Path()
-    p.add(0, 0)
-    p.add(10, 10)
-
-    p2 = Path()
-    p2.add(10, 0)
-    p2.add(0, 10)
+    p = Path.from_tuple_list([(0, 0), (10, 10)])
+    p2 = Path.from_tuple_list([(10, 0), (0, 10)])
 
     # classic cross intersection
 
@@ -456,13 +367,8 @@ def test_path_intersection1():
 
 
 def test_path_intersection2():
-    p = Path()
-    p.add(0, 0)
-    p.add(0, 10)
-
-    p2 = Path()
-    p2.add(1, 0)
-    p2.add(1, 10)
+    p = Path.from_tuple_list([(0, 0), (0, 10)])
+    p2 = Path.from_tuple_list([(1, 0), (1, 10)])
 
     # two parallel lines
 
@@ -471,13 +377,8 @@ def test_path_intersection2():
 
 
 def test_path_intersection3():
-    p = Path()
-    p.add(0, 0)
-    p.add(0, 10)
-
-    p2 = Path()
-    p2.add(1, 0)
-    p2.add(5, 10)
+    p = Path.from_tuple_list([(0, 0), (0, 10)])
+    p2 = Path.from_tuple_list([(1, 0), (5, 10)])
 
     # infinite lines will intersct
     # line segments not
@@ -487,11 +388,8 @@ def test_path_intersection3():
 
 
 def test_angles():
-    p = Path()
-    p.add(0.0, 1.0)
-    p.add(1.0, 1.0)
-    p.add(1.0, 0.0)
-    p.add(0.5, 0.0)
+    p = Path.from_tuple_list(
+        [(0, 1), (1, 1), (1, 0), (0.5, 0)])
 
     changes = p.direction_changes(mapped=True)
     assert changes[0] == 45.0
@@ -500,11 +398,8 @@ def test_angles():
 
 
 def test_angles_posneg():
-    p = Path()
-    p.add(0.0, 1.0)
-    p.add(1.0, 1.0)
-    p.add(1.0, 0.0)
-    p.add(1.0, 1.0)
+    p = Path.from_tuple_list(
+        [(0, 1), (1, 1), (1, 0), (1, 1)])
 
     changes = p.direction_changes(mapped=False)
     assert changes[0] == 45.0
@@ -513,42 +408,27 @@ def test_angles_posneg():
 
 
 def disabled_test_similarity():
-    p1 = Path()
-    p1.add(0, 0)
-    p1.add(0, 1)
-    p1.add(0, 2)
-    p1.add(0, 3)
-    p1.add(0, 4)
+    p1 = Path.from_tuple_list(
+        [(0, 0), (0, 1), (0, 2), (0, 3), (0, 4)])
 
-    p2 = Path()
-    p2.add(0, 0)
-    p2.add(0, 1)
-    p2.add(0, 2)
-    p2.add(0, 3)
-    p2.add(0, 4)
+    p2 = Path.from_tuple_list(
+        [(0, 0), (0, 1), (0, 2), (0, 3), (0, 4)])
 
     sim = p1.similarity(p2)
     assert sim == 1.0
 
-    p3 = Path()
-    p3.add(0, 0)
-    p3.add(0, 1)
-    p3.add(0, 2)
-    p3.add(0, 3.1)
-    p3.add(0, 4.1)
+    p3 = Path.from_tuple_list(
+        [(0, 0), (0, 1), (0, 2),
+         (0, 3.1), (0, 4.1)])
 
     sim2 = p1.similarity(p3)
     assert sim2 >= 0.9
 
 
 def test_offset():
-    p1 = Path()
-    p1.add(0, 0)
-    p1.add(1, 0)
-    p1.add(0.5, 1)
-    p1.add(3.5, 1)
-    p1.add(3, 0)
-    p1.add(4, 0)
+    p1 = Path.from_tuple_list(
+        [(0, 0), (1, 0), (0.5, 1),
+         (3.5, 1), (3, 0), (4, 0)])
 
     offset = p1.offset(0.2)
 
@@ -557,15 +437,9 @@ def test_offset():
 
 
 def test_downsample():
-    p1 = Path()
-    p1.add(0, 0)
-    p1.add(1, 0)
-    p1.add(2, 0)
-    p1.add(3, 0)
-    p1.add(4, 0)
-    p1.add(6, 0)
-    p1.add(7, 0)
-    p1.add(7.2, 0)
+    p1 = Path.from_tuple_list(
+        [(0, 0), (1, 0), (2, 0), (3, 0),
+         (4, 0), (6, 0), (7, 0), (7.2, 0)])
 
     p1.downsample(1.1)
 
@@ -573,21 +447,15 @@ def test_downsample():
 
 
 def test_rdp():
-    p1 = Path()
-    p1.add(0, 0)
-    p1.add(1, 0)
-    p1.add(2, 0)
-    p1.add(3, 0)
-    p1.add(4, 0)
-    p1.add(6, 0)
-    p1.add(7, 0)
-    p1.add(7.2, 0)
+    p = Path.from_tuple_list(
+        [(0, 0), (1, 0), (2, 0), (3, 0),
+         (4, 0), (6, 0), (7, 0), (7.2, 0)])
 
-    p1.simplify(0.1)
+    p.simplify(0.1)
 
-    assert len(p1) == 2
-    assert p1[0] == Position(0, 0)
-    assert p1[1] == Position(7.2, 0)
+    assert len(p) == 2
+    assert p[0] == Position(0, 0)
+    assert p[1] == Position(7.2, 0)
 
 
 def test_intersect():
@@ -603,20 +471,13 @@ def test_intersect():
 
 
 def test_clip():
-    p1 = Path()
-    p1.add(5, 5)
-    p1.add(5, 15)
-    p1.add(6, 15)
-    p1.add(6, 5)
-    p1.add(5, 5)
-    p1.add(11, 5)
-    p1.add(12, 5)
-    p1.add(5, 5)
-    p1.add(7, 5)
+    p = Path.from_tuple_list(
+        [(5, 5), (5, 15), (6, 15), (6, 5),
+         (5, 5), (11, 5), (12, 5), (5, 5), (7, 5)])
 
     bb = BoundingBox(1, 1, 10, 10)
 
-    clipped = p1.clip(bb)
+    clipped = p.clip(bb)
 
     assert len(clipped) == 3
     assert clipped[0][0] == Position(5, 5)
@@ -631,17 +492,14 @@ def test_clip():
     assert clipped[2][1] == Position(5, 5)
     assert clipped[2][2] == Position(7, 5)
 
-    assert p1[0] == Position(5, 5)
-    assert p1[1] == Position(5, 15)
-    assert p1[2] == Position(6, 15)
-    assert p1[3] == Position(6, 5)
+    assert p[0] == Position(5, 5)
+    assert p[1] == Position(5, 15)
+    assert p[2] == Position(6, 15)
+    assert p[3] == Position(6, 5)
 
 
 def test_arr():
-    p = Path()
-    p.add(0, 0)
-    p.add(1, 2)
-    p.add(3, 4)
+    p = Path.from_tuple_list([(0, 0), (1, 2), (3, 4)])
 
     arr = p.as_array()
     assert arr.shape[0] == 3
@@ -649,9 +507,7 @@ def test_arr():
 
 
 def test_centroid():
-    p = Path()
-    p.add(-1, -1)
-    p.add(1, 1)
+    p = Path.from_tuple_list([(-1, -1), (1, 1)])
 
     centroidx, centroidy = p.centeroid()
 
@@ -661,12 +517,7 @@ def test_centroid():
 
 def test_bb_mostly_inside():
     bb = BoundingBox(0, 0, 300, 300)
-    pa = Path()
-    pa.add(0, 0)
-    pa.add(0, 1)
-    pa.add(0, 3)
-    pa.add(-1, 3)
-    pa.add(-1, 5)
+    pa = Path.from_tuple_list([(0, 0), (0, 1), (0, 3), (-1, 3), (-1, 5)])
 
     assert pa.mostly_inside(bb)
 
@@ -676,10 +527,7 @@ def test_bb_mostly_inside():
 
 
 def test_is_closed():
-    pa = Path()
-    pa.add(0, 0)
-    pa.add(0, 1)
-    pa.add(0, 0)
+    pa = Path.from_tuple_list([(0, 0), (0, 1), (0, 0)])
 
     assert pa.is_closed()
 
@@ -689,32 +537,30 @@ def test_is_closed():
 
 
 def test_as_tuple_list():
-    pa = Path()
-    pa.add(0, 0)
-    pa.add(1, 2)
-    pa.add(3, 4)
+    pa = Path.from_tuple_list([(0, 0), (1, 2), (3, 4)])
 
     assert pa.as_tuple_list() == [(0, 0), (1, 2), (3, 4)]
 
 
 def test_from_tuple_list():
-    tuple_list = [(0, 0), (1, 2), (3, 4)]
-    pa = Path.from_tuple_list(tuple_list)
+    pa = Path.from_tuple_list([(0, 0), (1, 2), (3, 4)])
     assert pa[0] == Position(0, 0)
     assert pa[1] == Position(1, 2)
     assert pa[2] == Position(3, 4)
 
 
 def test_intersection_points():
-    p1 = Path()
-    p1.add(0, 0)
-    p1.add(1, 1)
-    p1.add(2, 2)
-    p1.add(0, 2)
-    p1.add(0, 1)
-    p1.add(1, 0)
+    p = Path.from_tuple_list([(0, 0), (1, 1), (2, 2), (0, 2), (0, 1), (1, 0)])
 
-    intersections = p1.intersection_points()
+    intersections = p.intersection_points()
     assert len(intersections) == 1
 
     assert intersections[0] == (0.5, 0.5)
+
+
+def test_move_to_origin():
+    p = Path.from_tuple_list([(10, 1), (10, 10), (10, 20)])
+
+    p.move_to_origin()
+
+    assert p.as_tuple_list() == [(0, 0), (0, 9), (0, 19)]
