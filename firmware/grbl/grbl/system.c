9034ffaef4b252a0c37721cbef3ec80be56a409b
Marcel Schwittlick <info@schwittlick.net>
2023-07-29 16:17:04 +0200

    disables input pins for door pen / feed hold stuff. weird but works to avoid going into feed hold when turning on flourescent lights

--- firmware/grbl/grbl/system.c
+++ firmware/grbl/grbl/system.c
@@ -70,14 +70,14 @@ ISR(CONTROL_INT_vect)
     if (bit_istrue(pin,CONTROL_PIN_INDEX_CYCLE_START)) {
       bit_true(sys_rt_exec_state, EXEC_CYCLE_START);
     }
-    #ifndef ENABLE_SAFETY_DOOR_INPUT_PIN
-      if (bit_istrue(pin,CONTROL_PIN_INDEX_FEED_HOLD)) {
-        bit_true(sys_rt_exec_state, EXEC_FEED_HOLD);
-    #else
-      if (bit_istrue(pin,CONTROL_PIN_INDEX_SAFETY_DOOR)) {
-        bit_true(sys_rt_exec_state, EXEC_SAFETY_DOOR);
-    #endif
-    }
+    //#ifndef ENABLE_SAFETY_DOOR_INPUT_PIN
+    //  if (bit_istrue(pin,CONTROL_PIN_INDEX_FEED_HOLD)) {
+    //    bit_true(sys_rt_exec_state, EXEC_FEED_HOLD);
+    //#else
+    //  if (bit_istrue(pin,CONTROL_PIN_INDEX_SAFETY_DOOR)) {
+    //    bit_true(sys_rt_exec_state, EXEC_SAFETY_DOOR);
+    //#endif
+   // }
   }
 }
 
