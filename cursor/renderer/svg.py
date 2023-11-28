3dd76cf7a904f09873ae111c24c5f25acfb7e192
Marcel Schwittlick <info@schwittlick.net>
2023-11-28 15:20:46 +0100

    fixes tests. rewrites svg renderer briefly

--- cursor/renderer/svg.py
+++ cursor/renderer/svg.py
@@ -5,57 +5,44 @@ import pathlib
 import svgwrite
 import wasabi
 
-from cursor.bb import BoundingBox
-from cursor.collection import Collection
-from cursor.path import Path
-from cursor.renderer import PathIterator
+from cursor.properties import Property
+from cursor.renderer import PathIterator, BaseRenderer
 
 log = wasabi.Printer()
 
 
-class SvgRenderer:
-    def __init__(self, folder: pathlib.Path):
-        self.save_path = folder
-        self.dwg = None
-        self.paths = Collection()
-        self.bbs = []
+class SvgRenderer(BaseRenderer):
+    def __init__(self, folder: pathlib.Path, w: int, h: int):
+        super().__init__(folder)
+        self.dwg = svgwrite.Drawing("", profile="tiny", size=(w, h))
 
-    def render(self, paths: Collection) -> None:
-        log.good(f"{__class__.__name__}: rendered {len(paths)} paths")
-        self.paths += paths
+    def render(self, scale: float = 1.0, frame: bool = False) -> None:
+        self.render_all_paths(scale=scale)
+        # self.render_all_points(scale=scale)
 
-    def render_bb(self, bb: BoundingBox) -> None:
-        self.bbs.append(bb)
+        # it = PathIterator(self.paths)
+        # for conn in it.connections():
+        #    line = self.dwg.line(
+        #        conn[0].as_tuple(),
+        #        conn[1].as_tuple(),
+        #        stroke_width=0.5,
+        #        stroke="black",
+        #    )
+        #    self.dwg.add(line)
 
-        p1 = Path()
-        p1.add(bb.x, bb.y)
-        p1.add(bb.x2, bb.y)
-        p1.add(bb.x2, bb.y2)
-        p1.add(bb.x, bb.y2)
-        p1.add(bb.x, bb.y)
-
-        self.paths.add(p1)
+    def render_all_paths(self, scale: float = 1.0):
+        for path in self.paths:
+            path.scale(scale, scale)
+            points = path.as_tuple_list()
+            color = path.properties[Property.COLOR]
+            width = path.properties[Property.WIDTH]
+            int_points = [tuple(map(str, p)) for p in points]
+            self.dwg.add(self.dwg.polyline(int_points, stroke_width=width, stroke=f"rgb{color}"))
 
     def save(self, filename: str) -> None:
-        bb = self.paths.bb()
-
+        self.dwg.filename = (self.save_path / (filename + ".svg")).as_posix()
         pathlib.Path(self.save_path).mkdir(parents=True, exist_ok=True)
 
-        fname = self.save_path / (filename + ".svg")
-        self.dwg = svgwrite.Drawing(fname.as_posix(), profile="tiny", size=(bb.w, bb.h))
+        self.dwg.save(pretty=True)
 
-        it = PathIterator(self.paths)
-        for conn in it.connections():
-            line = self.dwg.line(
-                conn[0].as_tuple(),
-                conn[1].as_tuple(),
-                stroke_width=0.5,
-                stroke="black",
-            )
-            self.dwg.add(line)
-
-        pathlib.Path(self.save_path).mkdir(parents=True, exist_ok=True)
-
-        log.good(f"Finished saving {fname}")
-
-        self.dwg.save()
+        log.good(f"Finished saving {self.dwg.filename}")
