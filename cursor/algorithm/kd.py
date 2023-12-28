b344b562b0abd06844fed5490386b69c071ecb8d
Marcel Schwittlick <info@schwittlick.net>
2023-12-28 13:50:25 +0100

    fix lint

--- cursor/algorithm/kd.py
+++ cursor/algorithm/kd.py
@@ -24,9 +24,8 @@ class KDTree:
         """
 
         if dist_sq_func is None:
-            def dist_sq_func():
-                return lambda a, b: sum((x - b[i]) ** 2
-                                        for i, x in enumerate(a))
+            dist_sq_func = lambda a, b: sum((x - b[i]) ** 2
+                                            for i, x in enumerate(a))
 
         def make(points, i=0):
             if len(points) > 1:
