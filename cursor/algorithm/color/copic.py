af8dc85b26de9e4f9c33a106f6bd20c88cbd0cf7
Marcel Schwittlick <info@schwittlick.net>
2023-12-28 20:48:35 +0100

    all kindsa color code

--- cursor/algorithm/color/copic.py
+++ cursor/algorithm/color/copic.py
@@ -3,27 +3,60 @@ import random
 import pathlib
 import colour
 
-from cursor.algorithm.color.copic_pen_enum import CopicColorCode
+from cursor.algorithm.color.copic_pen_enum import CopicColorCode as CCC
 
 
-class CopicColor:
-    def __init__(self, code: CopicColorCode, name: str, rgb: tuple[float, float, float]):
+class Color:
+    def __init__(self, rgb: tuple[float, float, float], code: CCC = None, name: str = "NoName"):
         self.code = code
         self.name = name
-        self.rgb = rgb
+        self.__rgb = rgb
+
+    def is_copic(self) -> bool:
+        return True if self.code else False
 
     def __repr__(self):
-        return f"{self.code.name}: {self.name}"
+        if self.code:
+            return f"{self.code.name}: {self.name}"
+        else:
+            return f"{self.__rgb}"
 
-    def as_srgb(self) -> tuple[float, float, float]:
-        return self.rgb[0] / 255, self.rgb[1] / 255, self.rgb[2] / 255
+    def as_srgb(self) -> tuple[float, ...]:
+        return self.__rgb
+
+    def as_rgb(self) -> tuple[int, ...]:
+        return tuple(int(v * 255) for v in self.__rgb)
 
 
 class Copic:
     def __init__(self):
+        self.available_colors_pens = self.init_available_pens()
         self.available_colors = self.__parse_data()
 
-    def __parse_data(self) -> dict[CopicColorCode, CopicColor]:
+    def init_available_pens(self) -> list[CCC]:
+        pens = []
+        pens.extend([CCC._110])
+        pens.extend(
+            [CCC.B0000, CCC.B000, CCC.B00, CCC.B01, CCC.B02, CCC.B04, CCC.B05,
+             CCC.B06, CCC.B12, CCC.B14, CCC.B16, CCC.B18, CCC.B21, CCC.B23, CCC.B24,
+             CCC.B26, CCC.B28, CCC.B29, CCC.B32, CCC.B34, CCC.B37, CCC.B39, CCC.B41,
+             CCC.B45, CCC.B52, CCC.B60, CCC.B63, CCC.B66, CCC.B69, CCC.B79, CCC.B91,
+             CCC.B93, CCC.B95, CCC.B97, CCC.B99,
+             CCC.FB2])
+
+        pens.extend(
+            [CCC.Y00, CCC.Y02, CCC.Y04, CCC.Y06, CCC.Y08, CCC.Y11, CCC.Y13, CCC.Y15, CCC.Y17,
+             CCC.Y18, CCC.Y19, CCC.Y21, CCC.Y23, CCC.Y28, CCC.Y32, CCC.Y35, CCC.Y38,
+             CCC.FY1])
+
+        pens.extend(
+            [CCC.R00, CCC.R01, CCC.R02, CCC.R05, CCC.R08, CCC.R11, CCC.R12, CCC.R14, CCC.R17,
+             CCC.R20, CCC.R21, CCC.R22, CCC.R24, CCC.R27, CCC.R29, CCC.R30, CCC.R32, CCC.R35,
+             CCC.FRV1])
+
+        return pens
+
+    def __parse_data(self) -> dict[CCC, Color]:
         """
         parses all copic color information from a json data file
         only colors that are added to the ColorCode enum class are indexed
@@ -36,35 +69,36 @@ class Copic:
             copic_data = json.loads(all_data)
             for element in copic_data:
                 try:
-                    id = CopicColorCode[element["id"]]
+                    id = CCC[element["id"]]
+
+                    if id not in self.available_colors_pens:
+                        continue
+
                     rgb = str(element['rgb']).split(',')
-                    rgb_tup = int(rgb[0]), int(rgb[1]), int(rgb[2])
-                    copic_data_final[id] = CopicColor(id, element['name'], rgb_tup)
+                    rgb_tup = int(rgb[0]) / 255, int(rgb[1]) / 255, int(rgb[2]) / 255
+                    copic_data_final[id] = Color(rgb_tup, id, element['name'])
                 except KeyError:
                     # only adding colors that have been turned into a pen
                     pass
         return copic_data_final
 
-    def color(self, code: CopicColorCode) -> CopicColor:
+    def color(self, code: CCC) -> Color:
         return self.available_colors[code]
 
-    def random(self) -> CopicColor:
+    def random(self) -> Color:
         return random.choice(list(self.available_colors.values()))
 
-    def most_similar(self, c1_rgb: tuple[int, int, int]) -> CopicColor:
+    def most_similar(self, c1_rgb: tuple[float, float, float]) -> Color:
         """
-        put in a color in RGB (0-255)
+        put in a color in RGB (0-1)
         will return the most similar color of the available Copic colors
         the similarity is calculated in CIE 1931 color space
         """
-        color_srgb = c1_rgb[0] / 255, c1_rgb[1] / 255, c1_rgb[2] / 255
-
-        color_to_compare_cie = colour.sRGB_to_XYZ(color_srgb)
+        color_to_compare_cie = colour.sRGB_to_XYZ(c1_rgb)
 
         deltas = {}
         for copic_color_code, copic_color in Copic().available_colors.items():
-            copic_color_srgb = copic_color.as_srgb()
-            copic_color_cie = colour.sRGB_to_XYZ(copic_color_srgb)
+            copic_color_cie = colour.sRGB_to_XYZ(copic_color.as_srgb())
 
             # color difference in CIE2000 (Color Difference Formula)
             # Designed to quantify the perceptual difference between two colors.
