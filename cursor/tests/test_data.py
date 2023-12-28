9a5ad1e6d7b8d76c9819c00df121bec92da0f940
Marcel Schwittlick <info@schwittlick.net>
2023-12-28 13:33:17 +0100

    linting

--- cursor/tests/test_data.py
+++ cursor/tests/test_data.py
@@ -65,7 +65,7 @@ def test_json_encoder():
     recs = {"mouse": mouse_recordings, "keys": keyboard_recodings}
     compressor = JsonCompressor()
     enc = compressor.json_zip(j=recs)
-    assert 128 <= len(enc["base64(zip(o))"]) <= 142
+    assert 124 <= len(enc["base64(zip(o))"]) <= 142
 
 
 def test_json_decoder():
