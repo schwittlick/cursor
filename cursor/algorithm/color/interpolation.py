fd19e4ba012ed3cf082ed05733d138b6b20e0b6a
Marcel Schwittlick <info@schwittlick.net>
2024-01-10 09:21:53 +0100

    adds progressbar to color interpolation

--- cursor/algorithm/color/interpolation.py
+++ cursor/algorithm/color/interpolation.py
@@ -1,4 +1,5 @@
 import numpy as np
+from tqdm import tqdm
 
 from cursor import misc
 from cursor.algorithm.color.copic import Color, Copic
@@ -7,7 +8,7 @@ from cursor.algorithm.color.copic import Color, Copic
 class ColorMath:
     @staticmethod
     def linear_srgb_to_oklab(
-        c: tuple[float, float, float]
+            c: tuple[float, float, float]
     ) -> tuple[float, float, float]:
         l = 0.4122214708 * c[0] + 0.5363325363 * c[1] + 0.0514459929 * c[2]
         m = 0.2119034982 * c[0] + 0.6806995451 * c[1] + 0.1073969566 * c[2]
@@ -25,7 +26,7 @@ class ColorMath:
 
     @staticmethod
     def oklab_to_linear_srgb(
-        c: tuple[float, float, float]
+            c: tuple[float, float, float]
     ) -> tuple[float, float, float]:
         l_ = c[0] + 0.3963377774 * c[1] + 0.2158037573 * c[2]
         m_ = c[0] - 0.1055613458 * c[1] - 0.0638541728 * c[2]
@@ -43,9 +44,9 @@ class ColorMath:
 
     @staticmethod
     def lerp(
-        v0: tuple[float, float, float],
-        v1: tuple[float, float, float],
-        percentage: float,
+            v0: tuple[float, float, float],
+            v1: tuple[float, float, float],
+            percentage: float,
     ):
         x = (1 - percentage) * v0[0] + percentage * v1[0]
         y = (1 - percentage) * v0[1] + percentage * v1[1]
@@ -54,10 +55,10 @@ class ColorMath:
 
     @staticmethod
     def interpolate(
-        c1_srgb: tuple[float, float, float],
-        c2_srgb: tuple[float, float, float],
-        perc: float,
-        oklab: bool = True,
+            c1_srgb: tuple[float, float, float],
+            c2_srgb: tuple[float, float, float],
+            perc: float,
+            oklab: bool = True,
     ) -> tuple[float, float, float]:
         if oklab:
             c1_oklab = ColorMath.linear_srgb_to_oklab(c1_srgb)
@@ -72,24 +73,26 @@ class ColorMath:
 
     @staticmethod
     def calc_gradient(
-        c1: Color,
-        c2: Color,
-        steps: int = 100,
-        oklab: bool = True,
-        clamp_to_copic: bool = True,
+            c1: Color,
+            c2: Color,
+            steps: int = 100,
+            oklab: bool = True,
+            clamp_to_copic: bool = True,
     ) -> list[Color]:
         gradient_colors = []
 
-        for step in range(steps):
-            perc = misc.inv_lerp(0, steps, step)
-            srgb_interpolated = ColorMath.interpolate(
-                c1.as_srgb(), c2.as_srgb(), perc, oklab
-            )
-            if clamp_to_copic:
-                interpolated_color = Copic().most_similar(srgb_interpolated)
-            else:
-                interpolated_color = Color(srgb_interpolated)
+        with tqdm(total=steps) as pbar:
+            for step in range(steps):
+                pbar.update(1)
+                perc = misc.inv_lerp(0, steps, step)
+                srgb_interpolated = ColorMath.interpolate(
+                    c1.as_srgb(), c2.as_srgb(), perc, oklab
+                )
+                if clamp_to_copic:
+                    interpolated_color = Copic().most_similar(srgb_interpolated)
+                else:
+                    interpolated_color = Color(srgb_interpolated)
 
-            gradient_colors.append(interpolated_color)
+                gradient_colors.append(interpolated_color)
 
         return gradient_colors
