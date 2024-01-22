59517a826a2c3f23f936726daeedc6ef07935a8c
Marcel Schwittlick <info@schwittlick.net>
2024-01-22 10:03:45 +0100

    move octet

--- tools/octet/data.py
+++
@@ -1,6 +0,0 @@
-from cursor.data import DataDirHandler
-from cursor.loader import Loader
-
-recordings = DataDirHandler().recordings()
-_loader = Loader(directory=recordings, limit_files=1)
-all_paths = _loader.all_paths()
