e95e8cb574f57857be534d232be8f44a473b3926
Marcel Schwittlick <info@schwittlick.net>
2024-02-13 19:02:42 +0100

    updates simple rect example

--- examples/simple_rect.py
+++ examples/simple_rect.py
@@ -1,22 +1,22 @@
 from cursor.collection import Collection
-from cursor.path import Path
+from cursor.device import PlotterType
 from cursor.export import ExportWrapper
-from cursor.device import PlotterType, PaperSize
+from cursor.path import Path
 
 if __name__ == "__main__":
     c = Collection()
 
     p = Path.from_tuple_list([(0, 0), (1, 0), (1, 1), (0, 1), (0, 0)])
-
-    p.velocity = 100
+    p.pen_select = 1
+    p.velocity = 50
 
     c.add(p)
 
-    ExportWrapper().ex(
+    wrapper = ExportWrapper(
         c,
-        PlotterType.ROLAND_DXY1200_A3,
-        PaperSize.PHOTO_PAPER_240_178_LANDSCAPE,
-        10,
+        PlotterType.MUTOH_XP500_500x297mm,
+        0,
         "simple_rect_example",
-        "test",
-    )
+        "test")
+    wrapper.fit()
+    wrapper.ex()
