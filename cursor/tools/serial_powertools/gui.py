0ef0a0bbe6ea2114e3fe22822d18330926a96fb5
Marcel Schwittlick <info@schwittlick.net>
2024-08-05 18:34:52 +0200

    experiments with drinking (dots and lines)

--- cursor/tools/serial_powertools/gui.py
+++ cursor/tools/serial_powertools/gui.py
@@ -7,7 +7,7 @@ import dearpygui.dearpygui as dpg
 import serial
 
 from cursor.collection import Collection
-from cursor.device import PlotterHpglNames, MinmaxMapping, PlotterType
+from cursor.device import PlotterHpglNames, MinmaxMapping
 from cursor.hpgl import RESET_DEVICE, ABORT_GRAPHICS
 from cursor.path import Path
 from cursor.renderer.hpgl import HPGLRenderer
@@ -123,8 +123,15 @@ def generate_circled_line_gui() -> str:
 
 
 def generate_random_dot_walk() -> str:
-    x = random.randint(0, 10000)
-    y = random.randint(0, 10000)
+    plotter_model = "DXY-1200"
+
+    potential_plotters = PlotterHpglNames.names[plotter_model]
+    guessed_plotter = potential_plotters[0]  # a3 size is the expected default here
+
+    bb = MinmaxMapping.maps[guessed_plotter]
+
+    x = random.randint(bb.x, bb.x2)
+    y = random.randint(bb.y, bb.y2)
 
     data = "SP1;VS1;PU;"
     data += f"PA{x},{y};"
@@ -145,8 +152,9 @@ def generate_random_dot_walk() -> str:
     return data
 
 
-def generate_filled_square(inspector: SerialInspector) -> str:
-    plotter_model = dpg.get_value("plotter_model")
+def generate_filled_square_lines(inspector: SerialInspector) -> str:
+    plotter_model = "DXY-1200"
+
     potential_plotters = PlotterHpglNames.names[plotter_model]
     guessed_plotter = potential_plotters[0]  # a3 size is the expected default here
 
@@ -159,17 +167,50 @@ def generate_filled_square(inspector: SerialInspector) -> str:
     x = random.randint(bb.x, bb.x2 - w)
     y = random.randint(bb.y, bb.y2 - h)
 
-    data = "SP1;VS1;PU;"
+    data = "VS1;SP1;VS1;PU;"
     data += f"PA{x},{y};"
 
-    stepsize = 30
+    stepsize = 25  # 25 also ok?
+    repetitions = 3
+
+    for rep in range(repetitions):
+        for x_step in range(x, x + w, stepsize):
+            data += f"PA{x_step},{y};"
+            data += "PD;"
+            data += f"PA{x_step},{y + h};"
+            data += "PU;"
+
+    return data
+
+
+def generate_filled_square_dotted(inspector: SerialInspector) -> str:
+    plotter_model = "DXY-1200"
+
+    potential_plotters = PlotterHpglNames.names[plotter_model]
+    guessed_plotter = potential_plotters[0]  # a3 size is the expected default here
+
+    bb = MinmaxMapping.maps[guessed_plotter]
+
+    w = random.randint(100, 2000)
+    h = random.randint(100, 2000)
+
+    # subtract w/h so we don't go out of bounds
+    x = random.randint(bb.x, bb.x2 - w)
+    y = random.randint(bb.y, bb.y2 - h)
+
+    data = "VS1;SP1;VS1;PU;"
+    data += f"PA{x},{y};"
+
+    stepsize = 25  # 25 also ok?
+    repetition = 3
 
     for x_step in range(x, x + w, stepsize):
         for y_step in range(y, y + h, stepsize):
-            data += f"PA{x_step},{y_step};"
-            data += "PD;"
-            data += f"PA{x_step + 10},{y_step + 10};"
-            data += "PU;"
+            for rep in range(repetition):
+                data += f"PA{x_step},{y_step};"
+                data += "PD;"
+                data += f"PA{x_step + 10},{y_step + 10};"
+                data += "PU;"
 
     return data
 
@@ -238,8 +279,10 @@ def create_plotter_inspector_gui(inspector: SerialInspector):
                            callback=lambda: inspector.send_command(generate_circled_line_gui()))
             dpg.add_button(label="Random dot walk",
                            callback=lambda: inspector.send_command(generate_random_dot_walk()))
-            dpg.add_button(label="Fill square",
-                           callback=lambda: inspector.send_command(generate_filled_square(inspector)))
+            dpg.add_button(label="Fill square (dotted)",
+                           callback=lambda: inspector.send_command(generate_filled_square_dotted(inspector)))
+            dpg.add_button(label="Fill square (lines)",
+                           callback=lambda: inspector.send_command(generate_filled_square_lines(inspector)))
 
     ports = [port.device for port in serial.tools.list_ports.comports()]
     dpg.configure_item("serial_port_dropdown", items=ports)
