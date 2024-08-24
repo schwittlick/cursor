a914e18e33f20b05080496a3d2904c1b50f636cf
Marcel Schwittlick <info@schwittlick.net>
2024-08-24 18:52:19 +0200

    remove dither function

--- cursor/misc.py
+++ cursor/misc.py
@@ -1,10 +1,8 @@
 import inspect
 
-import cv2
-import numpy as np
 import numba as nb
+import numpy as np
 import pynput
-
 from shapely import LineString
 from shapely.affinity import affine_transform
 
@@ -163,94 +161,6 @@ def remap(i_min: float, i_max: float, o_min: float, o_max: float, v: float) -> f
         6.2 == remap(1, 5, 3, 7, 4.2)
     """
     return lerp(o_min, o_max, inv_lerp(i_min, i_max, v))
-
-
-class ditherModule(object):
-    def dither(self, img, method="floyd-steinberg", resize=False):
-        if resize:
-            img = cv2.resize(
-                img,
-                (np.int(0.5 * (np.shape(img)[1])), np.int(0.5 * (np.shape(img)[0]))),
-            )
-        if method == "simple2D":
-            img = cv2.copyMakeBorder(img, 1, 1, 1, 1, cv2.BORDER_REPLICATE)
-            rows, cols = np.shape(img)
-            out = cv2.normalize(img.astype("float"), None, 0.0, 1.0, cv2.NORM_MINMAX)
-            for i in range(1, rows - 1):
-                for j in range(1, cols - 1):
-                    # threshold step
-                    if out[i][j] > 0.5:
-                        err = out[i][j] - 1
-                        out[i][j] = 1
-                    else:
-                        err = out[i][j]
-                        out[i][j] = 0
-
-                    # error diffusion step
-                    out[i][j + 1] = out[i][j + 1] + (0.5 * err)
-                    out[i + 1][j] = out[i + 1][j] + (0.5 * err)
-
-            return out[1: rows - 1, 1: cols - 1]
-
-        elif method == "floyd-steinberg":
-            img = cv2.copyMakeBorder(img, 1, 1, 1, 1, cv2.BORDER_REPLICATE)
-            rows, cols = np.shape(img)
-            out = cv2.normalize(img.astype("float"), None, 0.0, 1.0, cv2.NORM_MINMAX)
-            for i in range(1, rows - 1):
-                for j in range(1, cols - 1):
-                    # threshold step
-                    if out[i][j] > 0.9:
-                        err = out[i][j] - 1
-                        out[i][j] = 1
-                    else:
-                        err = out[i][j]
-                        out[i][j] = 0
-
-                    # error diffusion step
-                    out[i][j + 1] = out[i][j + 1] + ((7 / 16) * err)
-                    out[i + 1][j - 1] = out[i + 1][j - 1] + ((3 / 16) * err)
-                    out[i + 1][j] = out[i + 1][j] + ((5 / 16) * err)
-                    out[i + 1][j + 1] = out[i + 1][j + 1] + ((1 / 16) * err)
-
-            return out[1: rows - 1, 1: cols - 1]
-
-        elif method == "jarvis-judice-ninke":
-            img = cv2.copyMakeBorder(img, 2, 2, 2, 2, cv2.BORDER_REPLICATE)
-            rows, cols = np.shape(img)
-            out = cv2.normalize(img.astype("float"), None, 0.0, 1.0, cv2.NORM_MINMAX)
-            for i in range(2, rows - 2):
-                for j in range(2, cols - 2):
-                    # threshold step
-                    if out[i][j] > 0.5:
-                        err = out[i][j] - 1
-                        out[i][j] = 1
-                    else:
-                        err = out[i][j]
-                        out[i][j] = 0
-
-                    # error diffusion step
-                    out[i][j + 1] = out[i][j + 1] + ((7 / 48) * err)
-                    out[i][j + 2] = out[i][j + 2] + ((5 / 48) * err)
-
-                    out[i + 1][j - 2] = out[i + 1][j - 2] + ((3 / 48) * err)
-                    out[i + 1][j - 1] = out[i + 1][j - 1] + ((5 / 48) * err)
-                    out[i + 1][j] = out[i + 1][j] + ((7 / 48) * err)
-                    out[i + 1][j + 1] = out[i + 1][j + 1] + ((5 / 48) * err)
-                    out[i + 1][j + 2] = out[i + 1][j + 2] + ((3 / 48) * err)
-
-                    out[i + 2][j - 2] = out[i + 2][j - 2] + ((1 / 48) * err)
-                    out[i + 2][j - 1] = out[i + 2][j - 1] + ((3 / 48) * err)
-                    out[i + 2][j] = out[i + 2][j] + ((5 / 48) * err)
-                    out[i + 2][j + 1] = out[i + 2][j + 1] + ((3 / 48) * err)
-                    out[i + 2][j + 2] = out[i + 2][j + 2] + ((1 / 48) * err)
-
-            return out[2: rows - 2, 2: cols - 2]
-
-        else:
-            raise TypeError(
-                'specified method does not exist. available methods = "simple2D", \
-                "floyd-steinberg(default)", "jarvis-judice-ninke"'
-            )
 
 
 def current_source(frame):
