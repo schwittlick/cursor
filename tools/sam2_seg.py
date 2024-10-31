652ce7e1af0ab144a4da672bc6391a7c444df574
Marcel Schwittlick <info@schwittlick.net>
2024-10-31 16:37:20 +0100

    changing parameters

--- tools/sam2_seg.py
+++ tools/sam2_seg.py
@@ -138,11 +138,11 @@ mask_generator_2 = SAM2AutomaticMaskGenerator(
     pred_iou_thresh=0.7,
     stability_score_thresh=0.92,
     stability_score_offset=0.7,
-    crop_n_layers=1,
+    crop_n_layers=0,
     box_nms_thresh=0.7,
     crop_n_points_downscale_factor=2,
-    min_mask_region_area=25.0,
-    use_m2m=True,
+    min_mask_region_area=50,
+    use_m2m=False,
 )
 
 masks2 = mask_generator_2.generate(image)
