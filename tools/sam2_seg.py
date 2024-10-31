8eb93c5a7c1897af5d1f290db97ee582de89e7ae
Marcel Schwittlick <info@schwittlick.net>
2024-10-31 14:56:35 +0100

    rename folders

--- tools/sam2_seg.py
+++ tools/sam2_seg.py
@@ -86,7 +86,7 @@ from sam2.build_sam import build_sam2
 from sam2.automatic_mask_generator import SAM2AutomaticMaskGenerator
 
 sam2_checkpoint = DataDirHandler().data_dir / "sam2" / "checkpoints" / "sam2.1_hiera_large.pt"
-model_cfg = "sam2/configs/sam2.1/sam2.1_hiera_l.yaml"
+model_cfg = "sam2_cfg/configs/sam2.1/sam2.1_hiera_l.yaml"
 
 sam2 = build_sam2(model_cfg, sam2_checkpoint, device=device, apply_postprocessing=False)
 
