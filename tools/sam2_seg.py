f19dc25433722996fb43a5fabf0dab74e6f68358
Marcel Schwittlick <info@schwittlick.net>
2024-10-31 15:03:57 +0100

    fix paths

--- tools/sam2_seg.py
+++ tools/sam2_seg.py
@@ -85,8 +85,8 @@ image = np.array(image.convert("RGB"))
 from sam2.build_sam import build_sam2
 from sam2.automatic_mask_generator import SAM2AutomaticMaskGenerator
 
-sam2_checkpoint = DataDirHandler().data_dir / "sam2" / "checkpoints" / "sam2.1_hiera_large.pt"
-model_cfg = "./sam2_cfg/configs/sam2.1/sam2.1_hiera_l.yaml"
+sam2_checkpoint = "../checkpoints/sam2.1_hiera_large.pt"
+model_cfg = "configs/sam2.1/sam2.1_hiera_l.yaml"
 
 sam2 = build_sam2(model_cfg, sam2_checkpoint, device=device, apply_postprocessing=False)
 
