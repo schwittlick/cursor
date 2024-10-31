54100c2157dcbc1cc5b52f16c118dc1dafe59a94
Marcel Schwittlick <info@schwittlick.net>
2024-10-31 14:29:16 +0100

    Add sam2 segmentation

---
+++ tools/sam2.py
@@ -0,0 +1,96 @@
+import os
+
+from data import DataDirHandler
+from timer import Timer
+
+# if using Apple MPS, fall back to CPU for unsupported ops
+os.environ["PYTORCH_ENABLE_MPS_FALLBACK"] = "1"
+import numpy as np
+import torch
+import matplotlib.pyplot as plt
+from PIL import Image
+
+# select the device for computation
+if torch.cuda.is_available():
+    device = torch.device("cuda")
+elif torch.backends.mps.is_available():
+    device = torch.device("mps")
+else:
+    device = torch.device("cpu")
+print(f"using device: {device}")
+
+if device.type == "cuda":
+    # use bfloat16 for the entire notebook
+    torch.autocast("cuda", dtype=torch.bfloat16).__enter__()
+    # turn on tfloat32 for Ampere GPUs (https://pytorch.org/docs/stable/notes/cuda.html#tensorfloat-32-tf32-on-ampere-devices)
+    if torch.cuda.get_device_properties(0).major >= 8:
+        torch.backends.cuda.matmul.allow_tf32 = True
+        torch.backends.cudnn.allow_tf32 = True
+elif device.type == "mps":
+    print(
+        "\nSupport for MPS devices is preliminary. SAM 2 is trained with CUDA and might "
+        "give numerically different outputs and sometimes degraded performance on MPS. "
+        "See e.g. https://github.com/pytorch/pytorch/issues/84936 for a discussion."
+    )
+
+np.random.seed(3)
+
+
+def show_anns(anns, borders=True):
+    if len(anns) == 0:
+        return
+    sorted_anns = sorted(anns, key=(lambda x: x['area']), reverse=True)
+    ax = plt.gca()
+    ax.set_autoscale_on(False)
+
+    img = np.ones((sorted_anns[0]['segmentation'].shape[0], sorted_anns[0]['segmentation'].shape[1], 4))
+    img[:, :, 3] = 0
+    for ann in sorted_anns:
+        m = ann['segmentation']
+        color_mask = np.concatenate([np.random.random(3), [0.5]])
+        img[m] = color_mask
+        if borders:
+            import cv2
+            contours, _ = cv2.findContours(m.astype(np.uint8), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+            # Try to smooth contours
+            contours = [cv2.approxPolyDP(contour, epsilon=0.01, closed=True) for contour in contours]
+            cv2.drawContours(img, contours, -1, (0, 0, 1, 0.4), thickness=1)
+
+    ax.imshow(img)
+
+
+    plt.show()
+
+image = Image.open('images/cars.jpg')
+image = np.array(image.convert("RGB"))
+
+from sam2.build_sam import build_sam2
+from sam2.automatic_mask_generator import SAM2AutomaticMaskGenerator
+
+
+sam2_checkpoint = DataDirHandler().data_dir / "sam2" / "checkpoints" / "sam2.1_hiera_large.pt"
+model_cfg = "sam2/configs/sam2.1/sam2.1_hiera_l.yaml"
+
+sam2 = build_sam2(model_cfg, sam2_checkpoint, device=device, apply_postprocessing=False)
+
+mask_generator_2 = SAM2AutomaticMaskGenerator(
+    model=sam2,
+    points_per_side=64,
+    points_per_batch=128,
+    pred_iou_thresh=0.7,
+    stability_score_thresh=0.92,
+    stability_score_offset=0.7,
+    crop_n_layers=1,
+    box_nms_thresh=0.7,
+    crop_n_points_downscale_factor=2,
+    min_mask_region_area=25.0,
+    use_m2m=True,
+)
+masks2 = mask_generator_2.generate(image)
+plt.figure(figsize=(20, 20))
+plt.imshow(image)
+#show_anns(masks2)
+plt.axis('off')
+#plt.show()
+
+plt.savefig(DataDirHandler().png("sam2") / f'sam2_{Timer.timestamp()}.png')
