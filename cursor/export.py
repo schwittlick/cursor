05f1e58156c89812bbe50be484cecf73d35cd8f7
Marcel Schwittlick <info@schwittlick.net>
2024-12-07 16:17:03 +0100

    remove scaling down in exporter

--- cursor/export.py
+++ cursor/export.py
@@ -8,7 +8,6 @@ from typing import Optional, Dict
 
 from cursor.bb import BoundingBox
 from cursor.algorithm.color.copic import Copic
-from cursor.algorithm.color.copic_pen_enum import CopicColorCode
 from cursor.collection import Collection
 from cursor.data import DataDirHandler
 from cursor.device import (
@@ -164,7 +163,7 @@ class Exporter:
             fname = self._generate_filename(layer)
             jpeg_folder = DataDirHandler().jpg(self.name)
             bb = self.collection.bb()
-            bb.scale(0.1)
+            # bb.scale(0.1)
             transformed = pc.transformed(BoundingBox(0, 0, bb.w, bb.h))
             jpeg_renderer = JpegRenderer(jpeg_folder, w=int(bb.w), h=int(bb.h))
             jpeg_renderer.background((255, 255, 255))
