a763640ec6861f579b2e9ce0c28fa0727b10a54c
Marcel Schwittlick <info@schwittlick.net>
2024-12-17 09:58:38 +0100

    update arduino code

--- tools/rgb_led/rgb_led.ino
+++ tools/rgb_led/rgb_led.ino
@@ -15,18 +15,30 @@ void setup() {
 void loop() {
   // when a complete message arrives, process the RGB values
   if (stringComplete) {
+    Serial.print("Received: ");
+    Serial.println(inputString);
+
+    inputString.trim();  // Remove any leading/trailing whitespace
+
     if (inputString.startsWith("RGB") && inputString.endsWith(";")) {
       // Remove "RGB" prefix and ";" suffix
-      String values = inputString.substring(5, inputString.length() - 1);
+      String values = inputString.substring(3, inputString.length() - 1);
+
+      Serial.print("Parsed values: ");
+      Serial.println(values);
 
       // parse the comma-separated values
       int firstComma = values.indexOf(',');
       int secondComma = values.indexOf(',', firstComma + 1);
 
       if (firstComma != -1 && secondComma != -1) {
-        int r = values.substring(0, firstComma).toInt();
-        int g = values.substring(firstComma + 1, secondComma).toInt();
-        int b = values.substring(secondComma + 1).toInt();
+        String rString = values.substring(0, firstComma);
+        String gString = values.substring(firstComma + 1, secondComma);
+        String bString = values.substring(secondComma + 1);
+
+        int r = rString.toInt();
+        int g = gString.toInt();
+        int b = bString.toInt();
 
         // constrain values between 0 and 255
         r = constrain(r, 0, 255);
@@ -42,6 +54,8 @@ void loop() {
         Serial.print(g);
         Serial.print(",");
         Serial.println(b);
+      } else {
+        Serial.println("Invalid format: Couldn't find commas");
       }
     } else {
       Serial.println("Invalid format. Use: RGB255,0,0;");
@@ -56,12 +70,11 @@ void loop() {
 void serialEvent() {
   while (Serial.available()) {
     char inChar = (char)Serial.read();
+    inputString += inChar;
 
     // if the incoming character is a semicolon, set a flag
     if (inChar == ';') {
       stringComplete = true;
-    } else {
-      inputString += inChar;
     }
   }
 }
