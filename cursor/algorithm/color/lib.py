3288546bfbe0306ec47ef867b448f4d3b2bda360
Marcel Schwittlick <info@schwittlick.net>
2024-12-19 12:09:00 +0100

    adds weird legende scale thing that is now unused

--- cursor/algorithm/color/lib.py
+++ cursor/algorithm/color/lib.py
@@ -55,36 +55,36 @@ def create_legend_path(x: float, y: float, layer_index: int, pen_index: int, pat
     return legend_path
 
 
-def add_legende_all_corners(collection_bb, layer_index, c, pen_index, path_color):
+def add_legende_all_corners(collection_bb, layer_index, c, pen_index, path_color, legende_scale):
     num_legend_points = 3
 
-    x = collection_bb.x2 + layer_index * 2  # left side for legende
-    y = collection_bb.y2 - pen_index * 2
+    x = collection_bb.x2 + layer_index * 2 * legende_scale  # left side for legende
+    y = collection_bb.y2 - pen_index * 2 * legende_scale
     for _ in range(num_legend_points):
         legend_path = create_legend_path(x, y, layer_index, pen_index, path_color)
         c.add(legend_path)
 
-    x = collection_bb.x2 + layer_index * 2  # left side for legende
-    y = collection_bb.y + pen_index * 2
+    x = collection_bb.x2 + layer_index * 2 * legende_scale  # left side for legende
+    y = collection_bb.y + pen_index * 2 * legende_scale
     for _ in range(num_legend_points):
         legend_path = create_legend_path(x, y, layer_index, pen_index, path_color)
         c.add(legend_path)
 
-    x = collection_bb.x + layer_index * 2  # legende on right side
-    y = collection_bb.y2 - pen_index * 2
+    x = collection_bb.x + layer_index * 2 * legende_scale  # legende on right side
+    y = collection_bb.y2 - pen_index * 2 * legende_scale
     for _ in range(num_legend_points):
         legend_path = create_legend_path(x, y, layer_index, pen_index, path_color)
         c.add(legend_path)
 
-    x = collection_bb.x + layer_index * 2  # legende on right side
-    y = collection_bb.y + pen_index * 2
+    x = collection_bb.x + layer_index * 2 * legende_scale  # legende on right side
+    y = collection_bb.y + pen_index * 2 * legende_scale
     for _ in range(num_legend_points):
         legend_path = create_legend_path(x, y, layer_index, pen_index, path_color)
         c.add(legend_path)
 
 
 def sort_collection_by_copic_color_group(collection: Collection, legende_x: bool = False,
-                                         legende_y: bool = False) -> Collection:
+                                         legende_y: bool = False, legende_scale: int = 1) -> Collection:
     """
     the coordinates of the paths are in pixel space, not in hpgl/plotter space
     """
@@ -113,21 +113,21 @@ def sort_collection_by_copic_color_group(collection: Collection, legende_x: bool
         for path_color, paths_same_color in sorted_by_colors.items():
             color_names_pen_mapping[pen_index] = path_color.code
 
-            all_corners = True
+            all_corners = False
             if all_corners:
-                add_legende_all_corners(collection_bb, layer_index, c, pen_index, path_color)
+                add_legende_all_corners(collection_bb, layer_index, c, pen_index, path_color, legende_scale)
             else:
                 num_legend_points = 3
 
                 # adding legende of used colors
                 if legende_x:
-                    x = collection_bb.x2 + layer_index * 2  # left side for legende
+                    x = collection_bb.x2 + layer_index * 2 * legende_scale  # left side for legende
                 else:
-                    x = collection_bb.x + layer_index * 2  # legende on right side
+                    x = collection_bb.x + layer_index * 2 * legende_scale  # legende on right side
                 if legende_y:
-                    y = collection_bb.y2 - pen_index * 2
+                    y = collection_bb.y2 - pen_index * 2 * legende_scale
                 else:
-                    y = collection_bb.y + pen_index * 2
+                    y = collection_bb.y + pen_index * 2 * legende_scale
 
                 for _ in range(num_legend_points):
                     legend_path = create_legend_path(x, y, layer_index, pen_index, path_color)
