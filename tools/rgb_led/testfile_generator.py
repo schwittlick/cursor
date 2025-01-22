f0e9756e006ad1cc15a6e93d9255c746e6807b20
Marcel Schwittlick <info@schwittlick.net>
2025-01-22 17:39:15 +0100

    add mroe clib to rgb led tool

--- tools/rgb_led/testfile_generator.py
+++ tools/rgb_led/testfile_generator.py
@@ -1,11 +1,13 @@
 import sys
+import random
 
 from PyQt5.QtWidgets import QMainWindow, QApplication, QPushButton, QVBoxLayout, QWidget
 from PyQt5.QtCore import Qt
 
 from cursor import Collection, Path
-from device import PlotterType
+from device import PlotterType, MinmaxMapping
 from export import ExportWrapper
+from timer import Timer
 
 
 class MainWindow(QMainWindow):
@@ -24,6 +26,10 @@ class MainWindow(QMainWindow):
         self.generate_button = QPushButton("Generate Parallel Lines")
         self.generate_button.clicked.connect(self.generate_parallel_lines)
         layout.addWidget(self.generate_button, alignment=Qt.AlignCenter)
+
+        self.dot_pattern_button = QPushButton("Generate random dot test pattern")
+        self.dot_pattern_button.clicked.connect(self.random_dots)
+        layout.addWidget(self.dot_pattern_button, alignment=Qt.AlignCenter)
 
     def create_lines(self, color: tuple[int, int, int], offset_x: float, offset_y: float) -> Collection:
         pc = Collection()
@@ -77,6 +83,89 @@ class MainWindow(QMainWindow):
         wrappe.ex()
 
         print("Parallel lines generated and exported.")
+
+    def add_dot_block(self, xoff, yoff, color, padding_units):
+        dots = Collection()
+        for y in range(5):
+            for x in range(5):
+                path = Path()
+                path.pen_select = 1
+                path.velocity = 10
+                path.rgb_led_color = color
+                path.add(x * padding_units + xoff, y * padding_units + yoff)
+                path.add(x * padding_units + 1 + xoff, y * padding_units + 1 + yoff)
+                dots.add(path)
+        return dots
+
+    def general_dot_test_pattern(self):
+        dots = Collection()
+        intensity = 0.5
+
+        red = (255 * intensity, 0, 0)  # -> cyan
+        dots.extend(self.add_dot_block(0, 0, red, 10))  # Red
+        dots.extend(self.add_dot_block(200, 0, red, 20))  # Red
+        dots.extend(self.add_dot_block(500, 0, red, 30))  # Red
+        dots.extend(self.add_dot_block(900, 0, red, 40))  # Red
+        dots.extend(self.add_dot_block(1400, 0, red, 50))  # Red
+        dots.extend(self.add_dot_block(2000, 0, red, 50))  # Red
+        dots.extend(self.add_dot_block(2700, 0, red, 60))  # Red
+        dots.extend(self.add_dot_block(3400, 0, red, 70))  # Red
+        dots.extend(self.add_dot_block(4200, 0, red, 80))  # Red
+
+        green = (0, 255 * intensity, 0)  # -> magenta
+        dots.extend(self.add_dot_block(0, 1000, green, 10))
+        dots.extend(self.add_dot_block(200, 1000, green, 20))
+        dots.extend(self.add_dot_block(500, 1000, green, 30))
+        dots.extend(self.add_dot_block(900, 1000, green, 40))
+        dots.extend(self.add_dot_block(1400, 1000, green, 50))
+        dots.extend(self.add_dot_block(2000, 1000, green, 50))
+        dots.extend(self.add_dot_block(2700, 1000, green, 60))
+        dots.extend(self.add_dot_block(3400, 1000, green, 70))
+        dots.extend(self.add_dot_block(4200, 1000, green, 80))
+
+        blue = (0, 0, 255 * intensity)  # -> yellow
+        dots.extend(self.add_dot_block(0, 2000, blue, 10))
+        dots.extend(self.add_dot_block(200, 2000, blue, 20))
+        dots.extend(self.add_dot_block(500, 2000, blue, 30))
+        dots.extend(self.add_dot_block(900, 2000, blue, 40))
+        dots.extend(self.add_dot_block(1400, 2000, blue, 50))
+        dots.extend(self.add_dot_block(2000, 2000, blue, 50))
+        dots.extend(self.add_dot_block(2700, 2000, blue, 60))
+        dots.extend(self.add_dot_block(3400, 2000, blue, 70))
+        dots.extend(self.add_dot_block(4200, 2000, blue, 80))
+
+        wrapper = ExportWrapper(
+            dots,
+            PlotterType.ROLAND_DXY1200_A3,
+            10,
+            "rgb_led_plotter_testfile",
+            f"dot_test_pattern_low_intensity_{Timer.timestamp()}")
+        # wrapper.fit()
+        wrapper.ex()
+
+    def random_dots(self):
+        dots = Collection()
+
+        for i in range(1000):
+            path = Path()
+            path.pen_select = 1
+            path.velocity = 10
+            path.rgb_led_color = (random.uniform(0, 255), random.uniform(0, 255), random.uniform(0, 255))
+            bb = MinmaxMapping.maps[PlotterType.ROLAND_DXY1200_A3]
+            x, y = random.uniform(0, bb.w), random.uniform(0, bb.h)
+            path.add(x, y)
+            path.add(x + 1, y)
+            dots.add(path)
+
+        wrapper = ExportWrapper(
+            dots,
+            PlotterType.ROLAND_DXY1200_A3,
+            10,
+            "rgb_led_plotter_testfile",
+            f"random_dots{Timer.timestamp()}",
+            optimize=True)
+        # wrapper.fit()
+        wrapper.ex()
 
 
 def main():
