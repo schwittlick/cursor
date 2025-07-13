c05373e62acae3c0750f260c01f660c035179264
Marcel Schwittlick <info@schwittlick.net>
2025-07-13 17:17:05 +0200

    some updates to bitmap approximator

--- tools/bitmap_approximator/main.py
+++ tools/bitmap_approximator/main.py
@@ -7,5 +7,5 @@ plotter = PlotterType.HP_DM_RX_PLUS_A1
 
 if __name__ == '__main__':
     main_approximation()
-    # create_grid(plotter)
+    #create_grid(plotter)
     #create_fridge(plotter)
--- tools/bitmap_approximator/simple_style.py
+++ tools/bitmap_approximator/simple_style.py
@@ -8,11 +8,13 @@ import numpy as np
 from PIL import Image
 
 from bitmap_lib import do_bitmap_approximation
+from cursor import Position
 from cursor.collection import Collection
 from cursor.data import DataDirHandler
 from cursor.device import PlotterType
 from cursor.export import ExportWrapper
 from cursor.renderer.jpg import JpegRenderer
+from properties import Property
 
 
 def main_approximation():
@@ -43,8 +45,8 @@ def main_approximation():
     # a1 = 672x512 (a bit tighter would be 788x600
     wrapper = ExportWrapper(
         collection,
-        PlotterType.HP_7550A_A4,  # PlotterType.HP_7550A_A4,HP_DM_RX_PLUS_A1
-        0,  # 25mm - 11mm # for postcards in a4, set padding to 0 and add legende on all four sides, use 25 for a1
+        PlotterType.HP_7550A_A3,  # PlotterType.HP_7550A_A4,HP_DM_RX_PLUS_A1
+        15,  # 25mm - 11mm # for postcards in a4, set padding to 0 and add legende on all four sides, use 25 for a1
         "color_interpolation",
         f"bitmap_approximator_{path.name}",
         keep_aspect_ratio=True,
@@ -69,6 +71,12 @@ def select_file() -> pathlib.Path:
 def export_jpg_preview(data: np.ndarray, collection: Collection, path: pathlib.Path) -> None:
     dir = DataDirHandler().jpg("color_interpolation")
     r = JpegRenderer(dir, w=data.shape[0], h=data.shape[1])
-    r.add(collection)
+    for pa in collection:
+        pos = Position(pa[0].x, pa[0].y)
+        col = pa.color
+        pos.color = col
+        pos.radius = 1
+        r.add(pos.copy())
+    # r.add(collection)
     r.render()
     r.save(f"bitmap_approx_{path.name}")
