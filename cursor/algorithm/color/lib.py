3e5e4883bfeed2e4616dec61855390061ae95f66
Marcel Schwittlick <info@schwittlick.net>
2025-10-04 12:53:17 +0200

    fix import and gitignore

--- cursor/algorithm/color/lib.py
+++ cursor/algorithm/color/lib.py
@@ -7,6 +7,9 @@ from cursor.collection import Collection
 from cursor.path import Path
 from cursor.properties import Property
 from cursor.position import Position
+import math
+
+from cursor.properties import Property
 
 
 def convert_color_coordinates_to_collection(
